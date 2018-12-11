393cc279f44a723c1b8e49f19c64ce784cf81eca
Reuven <retzion@gmail.com>
2018-12-10 22:28:18 -0700

    remove renounceOwnership

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -45,17 +45,6 @@ contract Ownable {
   }
 
   /**
-  * @dev Allows the current owner to relinquish control of the contract.
-  * @notice Renouncing to ownership will leave the contract without an owner.
-  * It will not be possible to call the functions with the `onlyOwner`
-  * modifier anymore.
-  */
-  function renounceOwnership() public onlyOwner {
-    emit OwnershipTransferred(_owner, address(0));
-    _owner = address(0);
-  }
-
-  /**
   * @dev Allows the current owner to transfer control of the contract to a newOwner.
   * @param newOwner The address to transfer ownership to.
   */
