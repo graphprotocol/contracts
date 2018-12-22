65d890553a9ec615f8c22f0fdf05dc4ef9f21f7e
Reuven <retzion@gmail.com>
2018-12-21 18:21:51 -0700

    governors are now called members

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -5,7 +5,7 @@ import "./Ownable.sol";
 contract Governance is Owned {
     
     /* 
-    * @title Graph DAO Governance contract
+    * @title Graph Contract Governance contract
     *
     * @author Bryant Eisenbach
     * @author Reuven Etzion
@@ -16,97 +16,92 @@ contract Governance is Owned {
     * governance process. In the v1 specification, governance will consist of a small committee
     * which enacts changes to the protocol via a multi-sig contract.
     * 
-    * Requirements ("Governance"/"DAO" contract):
-    * @req 01 Contract has one or more Governor(s), majority can vote to perform Governor actions
-    *   - Function to check Mutli-sig authorization
-    * @req 02 Make external call to minimumCurationStakingAmount in Staking contract as Governor
-    * @req 03 Make external call to minimumIndexingStakingAmount in Staking contract 
-    * @req 04 Make external call to maxIndexers in Staking contract 
-    * @req 05 Verify the goverance contract can upgrade itself to a second copy of the goverance contract (???)
+    * Requirements ("Governance" contract):
+    * @req 01 Multisig contract will be inheritted and own this contract
+    * @req 02 Governance can update Staking contract {owner, minimumCurationStakingAmount, 
+    *   minimumIndexingStakingAmount, maxIndexers}
+    * @req 03 Verify the goverance contract can upgrade itself to a second copy of the goverance contract (???)
     * ...
     * Version 2
     * @req 01 (V2) Change Mutli-sig to use a voting mechanism
-    *   - Contract has one or more Governor(s), majority can vote to perform Governor actions
+    *   - Contract has one or more Members, majority can vote to perform governed actions
     */
 
+    // @TODO: multi-sig to check authorization (by vote count)?
+
+    // @FEATURE: Add Owned contract to upgradableContracts?
+    // @FEATURE: Remove or disable ownership of Owned contract?
+
+
     /* STATE VARIABLES */
-    // ------------------------------------------------------
-    // List of governors
-    address[] private governors;
-    // OR...
-    // Single governor
-    // address private governor;
-    // ------------------------------------------------------
+    // List of governing members
+    address[] private members;
 
-    // List of addresses of deployed contracts to be owned
-    Owned[] internal governedContracts;
+    // List of addresses of upgradable contracts to be owned by the multisig
+    Owned[] internal upgradableContracts;
 
-    // Governor-only modifier
-    modifier onlyGovernor {
-        // V1 - Check that sender is a governor
+    /* Contract Constructor */
+    /* @PARAM _upgradableContracts (string) - List of addresses of deployed contracts to be owned */
+    constructor (Owned[] memory _upgradableContracts) public {
+        // Assign the contracts to be governed / owned
+        // @DEPLOYMENT: Contracts must be deployed in the correct order
+        if (_upgradableContracts.length > 0) upgradableContracts = _upgradableContracts;
+
+        // Sender will become the sole governing member
+        members.push(msg.sender);
+    }
+
+    // Member-only modifier
+    modifier onlyMember {
         bool pass = false;
-        for (uint i; i < governors.length; i++) {
-            if (governors[i] == msg.sender) pass = true;
+        for (uint i; i < members.length; i++) {
+            if (members[i] == msg.sender) pass = true;
         }
         require(pass);
         _;
-        // @TODO: V2 - Check concensus of all governors
     }
 
-    constructor (Owned[] memory _governed) public {
-        // Assign the contracts to be governed
-        // @DEPLOYMENT: Contracts must be deployed in the correct order
-        // more to come..
-        if (_governed.length > 0) governedContracts = _governed;
 
-        // Sender will become the sole governor
-        governors.push(msg.sender);
-    }
-
-    // Accept the transfer of ownership of the contracts in the governedContracts list
+    // Accept the transfer of ownership of the contracts in the upgradableContracts list
     function acceptOwnershipOfAllContracts () public {
-        // iterate through governedContracts and accept ownership (acceptOwnership)
+        // iterate through upgradableContracts and accept ownership (acceptOwnership)
     }
 
-    // Initiate the transfer of ownership of the contracts in the governedContracts list
-    function transferOwnershipOfAllContracts (address _newGoverner) public onlyGovernor {
+    // Initiate the transfer of ownership of the contracts in the upgradableContracts list
+    function transferOwnershipOfAllContracts (address _newGoverner) public onlyOwner {
         // iterate through governed contracts and transfer to the newGoverner
     }
     
-    // @FEATURE: Add Owned contract to governedContracts?
-
-    // @TODO: multi-sig function to check authorization (by vote count)?
-
-    // Get list of governors
-    function getGovernors () public view returns (address[] memory) {
-        return governors;
+    // Get list of members
+    function getmembers () public view returns (address[] memory) {
+        return members;
     }
 
-    // Add a governor
-    function addGovernor (address _newGovernor) public onlyGovernor {
+    // Add a member
+    function addMember (address _newMember) public onlyOwner {
         // Prevent saving a duplicate
         bool duplicate;
-        for (uint i = 0; i < governors.length; i++) {
-            if (governors[i] == _newGovernor) duplicate = true;
+        for (uint i = 0; i < members.length; i++) {
+            if (members[i] == _newMember) duplicate = true;
         }
         require(!duplicate);
 
-        // Add address to governors list
-        governors.push(_newGovernor);
+        // Add address to members list
+        members.push(_newMember);
     }
 
-    // Remove a governor
-    function removeGovernor (address _removedGovernor) public onlyGovernor {
+    // Remove a member
+    function removeMember (address _removedMember) public onlyOwner {
         // Sender cannot remove self
-        require(msg.sender != _removedGovernor);
+        require(msg.sender != _removedMember);
         
-        // Remove _removedGovernor from governors list
+        // Remove _removedMember from members list
         uint i = 0;
-        while (governors[i] != _removedGovernor) {
+        while (members[i] != _removedMember) {
             i++;
         }
-        governors[i] = governors[governors.length - 1];
-        governors.length--;
+        members[i] = members[members.length - 1];
+        members.length--;
     }
 
 }
