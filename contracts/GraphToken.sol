08a3d570183063e31375d8a43da39aecf2b163da
Reuven <retzion@gmail.com>
2019-02-01 10:14:57 -0700

    GraphToken contract

--- contracts/GraphToken.sol
+++ contracts/GraphToken.sol
@@ -50,12 +50,31 @@ contract GraphToken is
 
     /* Modifiers */
     // Only a treasurers address is allowed
-    modifier onlyTreasurer ();
+    modifier onlyTreasurer () {
+        bool isTreasurer = false;
+        for (uint i = 0; i < treasurers.length; i++) {
+            if (msg.sender == treasurers[i]) isTreasurer = true;
+        }
+        require(isTreasurer);
+        _;
+    }
     
     /* Init Graph Token contract */
     /* @param _governor <address> - Address of the multisig contract as Governor of this contract */
     /* @param _initialSupply <uint256> - Initial supply of Graph Tokens */
-    constructor (address _governor, uint256 _initialSupply) public Governed (_governor);
+    constructor (address _governor, uint256 _initialSupply) public Governed (_governor) {
+        totalSupply = _initialSupply * 10**uint(decimals); // Initial totalSupply
+        treasurers.push(_governor); // Governor (multisig) is initially the sole treasurer
+
+        // @question: Who should own the initial supply of tokens?
+        // @dev If `governor` owns the tokens, we need to transfer when governor changes
+        balances[msg.sender] = totalSupply; // Deployment address holds all tokens
+        emit Transfer(
+            address(0), // from
+            msg.sender, // to
+            totalSupply // value
+        );
+    }
     
     /* Graph Protocol Functions */
     /**
@@ -65,14 +84,35 @@ contract GraphToken is
      * @param account <address> - The account that will receive the created tokens.
      * @param value <uint256> - The amount that will be created.
      */
-    function mint (address _account, uint256 _value) external onlyTreasurer;
+    function mint (address _account, uint256 _value) external onlyTreasurer {
+        require(_account != address(0));
+
+        totalSupply += _value;
+        balances[_account] += _value;
+        emit Transfer(address(0), _account, _value);
+    }
 
     /**
      * @dev Burns a specific amount of tokens from the target address and decrements allowance
      * @param _account <address> - The to burn tokens for.
      * @param _value <uint256> - The amount that will be burnt.
      */
-    function burnFrom (address _account, uint256 _value) public;
+    function burnFrom (address _account, uint256 _value) public {
+
+        // check balance
+        require(_value <= balances[_account]);
+
+        // check allowances before burning someone else's tokens
+        if (msg.sender != _account) {
+            require(_value <= allowed[_account][msg.sender]); // check allowance
+            allowed[_account][msg.sender] -= _value;
+        }
+
+        // Adjust balances and emit
+        balances[_account] -= _value;
+        totalSupply -= _value;
+        emit Transfer(_account, address(0), _value);
+    }
 
     /* 
      * @notice Add a Treasurer to the treasurers list
@@ -80,7 +120,17 @@ contract GraphToken is
      *
      * @param _newTreasurer <address> - Address of the Treasurer to be added
      */
-    function addTreasurer (address _newTreasurer) public onlyGovernance;
+    function addTreasurer (address _newTreasurer) public onlyGovernance {
+        // Prevent saving a duplicate
+        bool duplicate;
+        for (uint i = 0; i < treasurers.length; i++) {
+            if (treasurers[i] == _newTreasurer) duplicate = true;
+        }
+        require(!duplicate);
+
+        // Add address to treasurers list
+        treasurers.push(_newTreasurer);
+    }
 
     /* 
      * @notice Remove a Treasurer from the treasurers mapping
@@ -88,7 +138,18 @@ contract GraphToken is
      *
      * @param _removedTreasurer <address> - Address of the Treasurer to be removed
      */
-    function removeTreasurer (address _removedTreasurer) public onlyGovernance;
+    function removeTreasurer (address _removedTreasurer) public onlyGovernance {
+        // Sender cannot remove self
+        require(msg.sender != _removedTreasurer);
+        
+        // Remove _removedTreasurer from treasurers list
+        uint i = 0;
+        while (treasurers[i] != _removedTreasurer) {
+            i++;
+        }
+        treasurers[i] = treasurers[treasurers.length - 1];
+        treasurers.length--;
+    }
 
     // ------------------------------------------------------------------------
     // Don't accept ETH
