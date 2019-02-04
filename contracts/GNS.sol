d8f9ce64b64b26fb891dc997444cde1ae4843686
shkfnly <ashoka.finley@gmail.com>
2019-02-04 11:57:42 -0800

    updating GNS interface

--- contracts/GNS.sol
+++ contracts/GNS.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.5.2;
 import "./Governed.sol";
 
 contract GNS is Governed {
-    
-    /* 
+
+    /*
     * @title Graph Name Service (GNS) contract
     *
     * @author Bryant Eisenbach
@@ -12,25 +12,25 @@ contract GNS is Governed {
     *
     * @notice Contract Specification:
     *
-    * Subgraph IDs : Subgraph definitions are immutable, even though the actual 
-    * data ingested may grow -- each subgraph manifest is hashed in its IPLD 
-    * canonical serialized form, to produce a unique ID. Nodes in the peer-to-peer 
-    * network use this ID to communicate who is indexing and caching what data, as 
-    * well as route queries through the network. The self-certifying nature of 
+    * Subgraph IDs : Subgraph definitions are immutable, even though the actual
+    * data ingested may grow -- each subgraph manifest is hashed in its IPLD
+    * canonical serialized form, to produce a unique ID. Nodes in the peer-to-peer
+    * network use this ID to communicate who is indexing and caching what data, as
+    * well as route queries through the network. The self-certifying nature of
     * subgraph IDs also make them useful for providing attestations and filing disputes.
-    * 
-    * Domains : Subgraph IDs can also be associated with a domain name in the Graph 
-    * Name Service (GNS) to provide a mutable reference to a subgraph. This can be 
+    *
+    * Domains : Subgraph IDs can also be associated with a domain name in the Graph
+    * Name Service (GNS) to provide a mutable reference to a subgraph. This can be
     * useful for writing more human readable queries, always querying the latest version
-    * of a subgraph, specifying relationships between subgraphs or mutably referencing a 
+    * of a subgraph, specifying relationships between subgraphs or mutably referencing a
     * subgraph in smart contracts.
     *
     * Deploying a subgraph to a domain also enables discoverability, as explorer UIs will
     * be built on top of the GNS.
     *
-    * Subdomains : An owner of a domain in the GNS may wish to deploy multiple subgraphs 
-    * to a single domain, and have them exist in separate namespaces. Sub-domains enable 
-    * this use case, and add an optional additional layer of namespacing beyond that 
+    * Subdomains : An owner of a domain in the GNS may wish to deploy multiple subgraphs
+    * to a single domain, and have them exist in separate namespaces. Sub-domains enable
+    * this use case, and add an optional additional layer of namespacing beyond that
     * already provided by the top level domains.
     *
     * See: https://github.com/graphprotocol/specs/tree/master/data-model for details.
@@ -43,12 +43,18 @@ contract GNS is Governed {
     * ...
     */
 
+    /* Events */
+
+    event subdomainAdded
+    event subgraphIdAdded
     /* Structs */
     struct Domain {
         address owner;
         mapping (bytes32 => bytes32) subgraphNamesToIds;
     }
 
+    // The subgraph ID is the manifest which is hashed, these IDs are unique
+    // Domaains which are also hashed are attached to subGraphIDs
     /* STATE VARIABLES */
     // Storage of Domain Names mapped to subgraphId's
     // @question - What are we mapping to here? The subgraphId's owner's address?
@@ -62,7 +68,19 @@ contract GNS is Governed {
     }
 
     /* Graph Protocol Functions */
+    modifier onlyDomainOwner;
 
     // WIP...
-     
+    // Add a subgraphID to register domain
+    function registerDomain (bytes32 _subgraphID, address _owner) external onlyGovernance;
+
+    // Add a subdomain to a subgraphId
+    function addSubdomain (bytes32 _domainSubgraphID, address _owner) external onlyDomainOwner;
+
+    // Update subGraphId of an existing subdomain
+    function updateSubdomain (bytes32 _domainSubgraphID, address _owner, bytes32 _subdomainName, bytes32 _subdomainSubraphId) external onlyDomainOwner;
+    function getSubdomainSubgraphId (bytes32 _domainSubraphId, bytes32 _subdomainName) external returns (bytes32 subdomainId);
+    function deleteSubdomain (bytes32 _domainsubgraphID,
+    function transferDomainOwnersip
+
 }
