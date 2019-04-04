3e02528d78b9a54ad458e18ef38e8ea3b7ea1044
Bryant Eisenbach <fubuloubu@gmail.com>
2019-04-04 19:16:13 -0400

    refactor: Made Bancor formulas internal

--- contracts/bancor/BancorFormula.sol
+++ contracts/bancor/BancorFormula.sol
@@ -200,7 +200,7 @@ contract BancorFormula {
 
         @return purchase return amount
     */
-    function calculatePurchaseReturn(uint256 _supply, uint256 _connectorBalance, uint32 _connectorWeight, uint256 _depositAmount) public view returns (uint256) {
+    function calculatePurchaseReturn(uint256 _supply, uint256 _connectorBalance, uint32 _connectorWeight, uint256 _depositAmount) internal view returns (uint256) {
         // validate input
         require(_supply > 0 && _connectorBalance > 0 && _connectorWeight > 0 && _connectorWeight <= MAX_WEIGHT);
 
@@ -234,7 +234,7 @@ contract BancorFormula {
 
         @return sale return amount
     */
-    function calculateSaleReturn(uint256 _supply, uint256 _connectorBalance, uint32 _connectorWeight, uint256 _sellAmount) public view returns (uint256) {
+    function calculateSaleReturn(uint256 _supply, uint256 _connectorBalance, uint32 _connectorWeight, uint256 _sellAmount) internal view returns (uint256) {
         // validate input
         require(_supply > 0 && _connectorBalance > 0 && _connectorWeight > 0 && _connectorWeight <= MAX_WEIGHT && _sellAmount <= _supply);
 
