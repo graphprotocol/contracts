f9be9e7d99e31ed8e4163fb18dff9b1cdd1e7223
Reuven <retzion@gmail.com>
2019-04-26 10:30:17 -0600

    Staking over 10k should fail

---
+++ test/rewards.test.js
@@ -0,0 +1,94 @@
+const { shouldFail } = require('openzeppelin-test-helpers');
+
+// contracts
+const GraphToken = artifacts.require("./GraphToken.sol")
+const Staking = artifacts.require("./Staking.sol")
+
+// helpers
+const GraphProtocol = require('../graphProtocol.js')
+const helpers = require('./lib/testHelpers')
+
+contract('Rewards protection', ([
+  deploymentAddress,
+  daoContract,
+  curationStaker,
+  ...accounts
+]) => {
+  /** 
+   * testing constants & variables
+   */
+  const minimumCurationStakingAmount = 100,
+    defaultReserveRatio = 500000,
+    minimumIndexingStakingAmount = 100,
+    maximumIndexers = 10,
+    slashingPercent = 10,
+    thawingPeriod = 60 * 60 * 24 * 7 // seconds
+  let deployedStaking,
+    deployedGraphToken,
+    initialTokenSupply = 1000000,
+    stakingAmount = 10001, // 1 over the max staking amt
+    tokensMintedForStaker = stakingAmount * 10,
+    subgraphIdHex = helpers.randomSubgraphIdHex(),
+    subgraphIdBytes = helpers.randomSubgraphIdBytes(subgraphIdHex),
+    gp
+
+  beforeEach(async () => {
+    // deploy GraphToken contract
+    deployedGraphToken = await GraphToken.new(
+      daoContract, // governor
+      initialTokenSupply, // initial supply
+      { from: deploymentAddress }
+    )
+    assert.isObject(deployedGraphToken, "Deploy GraphToken contract.")
+
+    // send some tokens to the staking account
+    const tokensForCurator = await deployedGraphToken.mint(
+      curationStaker, // to
+      tokensMintedForStaker, // value
+      { from: daoContract }
+    )
+    assert(tokensForCurator, "Mints Graph Tokens for Curator.")
+
+    // deploy Staking contract
+    deployedStaking = await Staking.new(
+      daoContract, // <address> governor
+      minimumCurationStakingAmount, // <uint256> minimumCurationStakingAmount
+      defaultReserveRatio, // <uint256> defaultReserveRatio
+      minimumIndexingStakingAmount, // <uint256> minimumIndexingStakingAmount
+      maximumIndexers, // <uint256> maximumIndexers
+      slashingPercent, // <uint256> slashingPercent
+      thawingPeriod, // <uint256> thawingPeriod
+      deployedGraphToken.address, // <address> token
+      { from: deploymentAddress }
+    )
+    assert.isObject(deployedStaking, "Deploy Staking contract.")
+
+    // init Graph Protocol JS library with deployed staking contract
+    gp = GraphProtocol({
+      Staking: deployedStaking,
+      GraphToken: deployedGraphToken
+    })
+    assert.isObject(gp, "Initialize the Graph Protocol library.")
+  })
+
+  it('...should not allow staking of more than 10,000 shares', async () => {
+    let totalBalance = await deployedGraphToken.balanceOf(deployedStaking.address)
+    let curatorBalance = await deployedGraphToken.balanceOf(curationStaker)
+    assert(
+      curatorBalance.toNumber() === tokensMintedForStaker && 
+      totalBalance.toNumber() === 0,
+      "Balances before transfer are correct."
+    )
+
+    const data = web3.utils.hexToBytes('0x01' + subgraphIdHex)
+    const curationStake = deployedGraphToken.transferWithData(
+      deployedStaking.address, // to
+      stakingAmount, // value
+      data, // data
+      { from: curationStaker }
+    )
+
+    // assert a failure due to `stakingAmount` being too high
+    await shouldFail.reverting( curationStake )
+  })
+})
