f40bca65073e5d8fafdf7ee2c4569dfc89da8336
Jannis Pohlmann <jannis@thegraph.com>
2019-09-12 17:31:18 +0200

    contracts, test: Simplify indexer URLs in ServiceRegistry

--- test/serviceRegisty.test.js
+++ test/serviceRegisty.test.js
@@ -19,13 +19,11 @@ contract('Service Registry', accounts => {
 
   it('...should allow setting URL with arbitrary length string', async () => {
     const url = 'https://192.168.2.1/'
-    const urlBytes = web3.utils.utf8ToHex(url)
 
     // Set the url
-    const { logs } = await deployedServiceRegistry.setUrl(
-      url,
-      { from: indexer },
-    )
+    const { logs } = await deployedServiceRegistry.setUrl(url, {
+      from: indexer,
+    })
 
     expectEvent.inLogs(logs, 'ServiceUrlSet', {
       serviceProvider: indexer,
@@ -34,14 +32,13 @@ contract('Service Registry', accounts => {
   })
 
   it('...should allow setting URL with a very long string', async () => {
-    const url = 'https://aaaaanksgrhlqghqrefgerqfgnsqgjklsohfdhjkfkjsdhrhesrhkfshkfehkusefhkjesjhfsehjfhkserhkjsehkrhesjkrhsjhrshjkerhjkshjerhjkse.com'
-    const urlBytes = web3.utils.utf8ToHex(url)
+    const url =
+      'https://aaaaanksgrhlqghqrefgerqfgnsqgjklsohfdhjkfkjsdhrhesrhkfshkfehkusefhkjesjhfsehjfhkserhkjsehkrhesjkrhsjhrshjkerhjkshjerhjkse.com'
 
     // Set the url
-    const { logs } = await deployedServiceRegistry.setUrl(
-      url,
-      { from: indexer },
-    )
+    const { logs } = await deployedServiceRegistry.setUrl(url, {
+      from: indexer,
+    })
 
     expectEvent.inLogs(logs, 'ServiceUrlSet', {
       serviceProvider: indexer,
@@ -49,45 +46,34 @@ contract('Service Registry', accounts => {
     })
   })
 
-
   it('...should allow setting multiple graph network service providers URL, getting the length, and removing an indexer', async () => {
     const url = 'https://192.168.2.1/'
     const urlBytes = web3.utils.utf8ToHex(url)
-    const indexers = accounts.slice(5,8)
+    const indexers = accounts.slice(5, 8)
     for (let i = 0; i < 3; i++) {
-
       // Set the url, only governor can
-      const { logs } = await deployedServiceRegistry.setGraphNetworkServiceProviderURLs(
+      const {
+        logs,
+      } = await deployedServiceRegistry.setGraphNetworkServiceProviderURL(
         indexers[i],
         url,
         { from: governor },
       )
 
+      // Verify that the the ServiceUrlSet event is emitted
       expectEvent.inLogs(logs, 'ServiceUrlSet', {
         serviceProvider: indexers[i],
         urlString: url,
       })
-    }
 
-    const indexersSetLength = await deployedServiceRegistry.numberOfGraphNetworkServiceProviderURLs()
-    assert(indexersSetLength.toNumber() === indexers.length, 'The amount of indexers are not matching.')
-
-    // Remove a URL, only governor can
-    await deployedServiceRegistry.removeGraphNetworkIndexerURL(
-      indexers[1],
-      { from: governor },
-    )
-
-    for (let i = 0; i < 3; i++) {
-      let serviceProvider = await deployedServiceRegistry.graphNetworkServiceProviderURLs(i)
-      if (i == 1) {
-        assert(serviceProvider.indexer === helpers.zeroAddress() , `Indexer address ${i} was not deleted .`)
-        assert(null === serviceProvider.url, `Indexer url ${i} was not deleted.`)
-      } else {
-        assert(serviceProvider.indexer === indexers[i], `Indexer address ${i} does not match.`)
-        assert(web3.utils.utf8ToHex(url) === serviceProvider.url, `Indexer url ${i} does not match.`)
-      }
+      // Verify that the indexer URL has been updated
+      let indexerUrlBytes = await deployedServiceRegistry.graphNetworkServiceProviderURLs(
+        indexers[i],
+      )
+      assert(
+        indexerUrlBytes === urlBytes,
+        `Indexer ${i} URL was not set to ${url}`,
+      )
     }
   })
-
 })
