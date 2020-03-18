441817995b5f62c1505a22ef28715538226a51e0
Ariel Barmat <abarmat@gmail.com>
2020-03-18 18:32:55 -0300

    build: update to Solidity 6.4

--- contracts/GraphToken.sol
+++ contracts/GraphToken.sol
@@ -1,34 +1,17 @@
-pragma solidity ^0.5.2;
+pragma solidity ^0.6.4;
+
 /*
  * @title GraphToken contract
- *
- * @author Bryant Eisenbach
- * @author Reuven Etzion
- *
- * Requirements
- * @req 01 The Graph Token shall implement the ERC20 Token Standard
- * @req 02 The Graph Token shall allow tokens staked in the protocol to be burned
- * @req 03 The Graph Token shall allow tokens to be minted to reward protocol participants
- * @req 04 The Graph Token shall only allow designated accounts the authority to mint
- *         Note: for example, the Payment Channel Hub and Rewards Manager contracts
- * @req 05 The Graph Token shall allow the protocol Governance to modify the accounts that have
- *         minting authority
- * @req 06 The Graph Token shall allow the protocol Governance to mint new tokens
- * @req 07 The Graph Token shall mint an inital distribution of tokens
- * @req 08 The Graph Token shall allow a token holder to stake in the protocol for indexing or
- *         curation markets for a particular Subgraph
  *
  */
 
 import "./Governed.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
-
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 // @imp 08 target _to of transfer(_to, _amount, _data) in Token must implement this interface
 // NOTE: This is based off of ERC777TokensRecipient interface, but does not fully implement it
-contract TokenReceiver {
+interface TokenReceiver {
     function tokensReceived(
         address _from,
         uint256 _amount,
@@ -36,18 +19,17 @@ contract TokenReceiver {
     ) external returns (bool);
 }
 
+contract GraphToken is Governed, ERC20Detailed, ERC20Burnable {
+    mapping(address => bool) private _minters;
 
-contract GraphToken is
-    Governed,
-    ERC20Detailed, // @imp 01
-    ERC20Burnable, // @imp 01, 02
-    ERC20Mintable // @imp 01, 03, 04
-{
-    // @imp 05, 06 Override so Governor can set Minters or mint tokens
+    // -- Events --
+    event MinterAdded(address indexed account);
+    event MinterRemoved(address indexed account);
+
     modifier onlyMinter() {
         require(
             isMinter(msg.sender) || msg.sender == governor,
-            "Only minter can call."
+            "Only minter can call"
         );
         _;
     }
@@ -62,20 +44,36 @@ contract GraphToken is
         ERC20Detailed("Graph Token", "GRT", 18)
         Governed(_governor)
     {
-        // @imp 06 Governor is initially the sole treasurer
+        // Governor is initially the sole treasurer
         _addMinter(_governor);
-        _removeMinter(msg.sender); // Zep automagically does this, so remove...
 
-        // @imp 07 The Governer has the initial supply of tokens
-        _mint(_governor, _initialSupply); // Deployment address holds all tokens
+        // The Governor has the initial supply of tokens
+        _mint(_governor, _initialSupply);
     }
 
-    /**
-     * @dev Method to expose `removeToken` while using the `onlyGovernor` modifier
-     * @param _account <address> Address of account to remove from `_minters`
-     */
-    function removeMinter(address _account) public onlyGovernance {
+    function addMinter(address _account) external onlyGovernance {
+        _addMinter(_account);
+    }
+
+    function removeMinter(address _account) external onlyGovernance {
         _removeMinter(_account);
+    }
+
+    function isMinter(address _account) public view returns (bool) {
+        return _minters[_account];
+    }
+
+    function renounceMinter() external {
+        _removeMinter(msg.sender);
+    }
+
+    function mint(address _account, uint256 _amount)
+        external
+        onlyMinter
+        returns (bool)
+    {
+        _mint(_account, _amount);
+        return true;
     }
 
     /*
@@ -92,5 +90,15 @@ contract GraphToken is
         // @imp 08 Have staking contract receive the token and handle the data
         assert(TokenReceiver(_to).tokensReceived(msg.sender, _amount, _data));
         success = true;
+    }
+
+    function _addMinter(address _account) internal {
+        _minters[_account] = true;
+        emit MinterAdded(_account);
+    }
+
+    function _removeMinter(address _account) internal {
+        _minters[_account] = false;
+        emit MinterRemoved(_account);
     }
 }
