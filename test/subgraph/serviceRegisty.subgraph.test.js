1a285669bec9f7b72fc532fb43b718fff5d0a120
Ariel Barmat <abarmat@gmail.com>
2020-05-12 20:08:52 -0300

    service-registry: comment out tests

--- test/subgraph/serviceRegisty.subgraph.test.js
+++ test/subgraph/serviceRegisty.subgraph.test.js
@@ -1,33 +1,33 @@
-const { expectEvent } = require('@openzeppelin/test-helpers')
+// const { expectEvent } = require('@openzeppelin/test-helpers')
 
-// contracts
-const ServiceRegisty = artifacts.require('./ServiceRegistry.sol')
-const helpers = require('../lib/testHelpers')
+// // contracts
+// const ServiceRegisty = artifacts.require('./ServiceRegistry.sol')
+// const helpers = require('../lib/testHelpers')
 
-contract('Service Registry', accounts => {
-  let deployedServiceRegistry
+// contract('Service Registry', accounts => {
+//   let deployedServiceRegistry
 
-  before(async () => {
-    // deploy ServiceRegistry contract
-    deployedServiceRegistry = await ServiceRegisty.new(
-      accounts[0], // governor
-      { from: accounts[0] },
-    )
-  })
+//   before(async () => {
+//     // deploy ServiceRegistry contract
+//     deployedServiceRegistry = await ServiceRegisty.new(
+//       accounts[0], // governor
+//       { from: accounts[0] },
+//     )
+//   })
 
-  it('...should allow setting URL 10 times', async () => {
-    for (let i = 0; i < 10; i++) {
-      const url = helpers.testServiceRegistryURLS[i]
+//   it('...should allow setting URL 10 times', async () => {
+//     for (let i = 0; i < 10; i++) {
+//       const url = helpers.testServiceRegistryURLS[i]
 
-      // Set the url
-      const { logs } = await deployedServiceRegistry.setUrl(url, {
-        from: accounts[i],
-      })
+//       // Set the url
+//       const { logs } = await deployedServiceRegistry.setUrl(url, {
+//         from: accounts[i],
+//       })
 
-      expectEvent.inLogs(logs, 'ServiceUrlSet', {
-        serviceProvider: accounts[i],
-        urlString: url,
-      })
-    }
-  })
-})
+//       expectEvent.inLogs(logs, 'ServiceUrlSet', {
+//         serviceProvider: accounts[i],
+//         urlString: url,
+//       })
+//     }
+//   })
+// })
