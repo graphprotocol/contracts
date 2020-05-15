0fa35c847f091c6e221cab00598c8dd504808067
Heiko Fisch <heiko.fisch@gmail.com>
2020-05-15 23:42:11 +0200

    Revert "connext: Remove DolphinCoin test token"
    This reverts commit 86ffd5611f1a6603167669d78f4c4997cfd80b12.
    Reverted commit is no longer relevant, as OpenZeppelin contracts
    are about to be upgraded to v3.0.1.

---
+++ contracts/connext/funding/test-fixtures/DolphinCoin.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.6.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract DolphinCoin is ERC20("DolphinCoin", "DOC") {
+    uint8 public constant DECIMALS = 18;
+    uint256 public constant INITIAL_SUPPLY = 10_000 * (uint256(10) ** DECIMALS);
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor() public {
+        _setupDecimals(DECIMALS);
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
