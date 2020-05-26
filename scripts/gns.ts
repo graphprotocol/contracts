eb5ab2f187a585f04e166839e80abcfbd49651d1
David Kajpust <kajpustd@gmail.com>
2020-05-26 16:25:35 -0400

    Finish gns scripts, and update helpers

---
+++ scripts/gns.ts
@@ -0,0 +1,154 @@
+#!/usr/bin/env ts-node
+
+import { utils } from "ethers";
+import * as path from "path";
+import * as minimist from "minimist";
+
+import { contracts, executeTransaction, overrides, IPFS } from "./helpers";
+
+///////////////////////
+// Set up the script //
+///////////////////////
+
+let { func, subgraphName, ipfs, subgraphID, metadataPath, newOwner } = minimist(process.argv.slice(2), {
+  string: ["func", "subgraphName", "ipfs", "subgraphID", "metadataPath", "newOwner"]
+});
+
+if (!func || !subgraphName) {
+  console.error(
+    `
+Usage: ${path.basename(process.argv[1])}
+    --func <text> - options: publish, unpublish, transfer
+
+Function arguments:
+    publish
+      --ipfs <url>            - ex. https://api.thegraph.com/ipfs/
+      --subgraphName <text>   - name of the subgraph
+      --subgraphID <base58>   - subgraphID in bas358
+      --metadata <path>       - filepath to metadata. JSON format:
+                                  {
+                                    "displayName": "",
+                                    "image": "",
+                                    "description": "",
+                                    "codeRepository": "",
+                                    "websiteURL": ""
+                                  }
+    
+    unpublish
+      --subgraphName <text>  - name of the subgraph
+
+    transfer
+      --subgraphName <text>  - name of the subgraph
+      --new-owner <address>   - address of the new owner
+`
+  );
+  process.exit(1);
+}
+
+///////////////////////
+// functions //////////
+///////////////////////
+
+const publish = async () => {
+  if (!ipfs || !subgraphID || !metadataPath) {
+    console.error(`ERROR: publish must be provided an ipfs endpoint and a subgraphID`);
+    process.exit(1);
+  }
+  console.log("Subgraph:      ", subgraphName);
+  console.log("Subgraph ID:   ", subgraphID);
+  console.log("IPFS:          ", ipfs);
+  console.log("Metadata path: ", metadataPath);
+
+  const metadata = require(metadataPath);
+  console.log("Meta data:");
+  console.log("  Display name:     ", metadata.displayName || "");
+  console.log("  Image:            ", metadata.image || "");
+  console.log("  Subtitle:         ", metadata.subtitle || "");
+  console.log("  Description:      ", metadata.description || "");
+  console.log("  Code Repository:  ", metadata.codeRepository || "");
+  console.log("  Website:          ", metadata.websiteURL || "");
+
+  let ipfsClient = IPFS.createIpfsClient(ipfs);
+
+  console.log("\nUpload JSON meta data to IPFS...");
+  let result = await ipfsClient.add(Buffer.from(JSON.stringify(metadata)));
+  let metaHash = result[0].hash;
+  let metaHashBytes = IPFS.ipfsHashToBytes32(metaHash);
+  try {
+    let data = JSON.parse(await ipfsClient.cat(metaHash));
+    if (JSON.stringify(data) !== JSON.stringify(metadata)) {
+      throw new Error(`Original meta data and uploaded data are not identical`);
+    }
+  } catch (e) {
+    throw new Error(`Failed to retrieve and parse JSON meta data after uploading: ${e.message}`);
+  }
+  console.log("Upload metadata successful!\n");
+
+  let subgraphIDBytes = IPFS.ipfsHashToBytes32(subgraphID);
+  const gnsOverrides = await overrides("gns", "publish");
+  try {
+    await executeTransaction(
+      contracts.gns.functions.publish(subgraphName, subgraphIDBytes, metaHashBytes, gnsOverrides)
+    );
+  } catch (e) {
+    console.log(`  ..failed: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+const unpublish = async () => {
+  console.log("Subgraph:           ", subgraphName);
+  let nameHash = utils.id(subgraphName);
+  console.log("Subgraph name hash: ", nameHash);
+  console.log("\n");
+  const gnsOverrides = await overrides("gns", "unpublish");
+  try {
+    await executeTransaction(contracts.gns.functions.unpublish(nameHash, gnsOverrides));
+  } catch (e) {
+    console.log(`  ..failed: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+const transfer = async () => {
+  if (!newOwner) {
+    console.error(`ERROR: transfer must be provided a new owner`);
+    process.exit(1);
+  }
+  console.log("Subgraph:           ", subgraphName);
+  let nameHash = utils.id(subgraphName);
+  console.log("Subgraph name hash: ", nameHash);
+  console.log("New owner:          ", newOwner);
+  console.log("\n");
+  const gnsOverrides = await overrides("gns", "unpublish");
+  try {
+    await executeTransaction(contracts.gns.functions.transfer(nameHash, newOwner, gnsOverrides));
+  } catch (e) {
+    console.log(`  ..failed: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+///////////////////////
+// main ///////////////
+///////////////////////
+
+const main = async () => {
+  try {
+    if (func == "publish") {
+      console.log(`Publishing subgraph ${subgraphName} ...`);
+      publish();
+    } else if (func == "unpublish") {
+      console.log(`Unpublishing subgraph ${subgraphName} ...`);
+      unpublish();
+    } else if (func == "transfer") {
+      console.log(`Transferring ownership of subgraph ${subgraphName} to ${newOwner}`);
+      transfer();
+    }
+  } catch (e) {
+    console.log(`  ..failed: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+main();
--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,62 +1,97 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import { ethers, utils, Wallet } from 'ethers'
-import * as ipfsHttpClient from 'ipfs-http-client'
-import * as bs58 from 'bs58'
+import * as fs from "fs";
+import * as path from "path";
 
-import { GNSFactory } from '../build/typechain/contracts/GNSFactory'
-import { StakingFactory } from '../build/typechain/contracts/StakingFactory'
-import { ServiceRegistryFactory } from '../build/typechain/contracts/ServiceRegistryFactory'
-import { GraphTokenFactory } from '../build/typechain/contracts/GraphTokenFactory'
+import { ContractTransaction, ethers, utils, Wallet } from "ethers";
+import { ContractReceipt } from "ethers/contract";
+import * as ipfsHttpClient from "ipfs-http-client";
+import * as bs58 from "bs58";
 
+import { GnsFactory } from "../build/typechain/contracts/GnsFactory";
+import { StakingFactory } from "../build/typechain/contracts/StakingFactory";
+import { ServiceRegistryFactory } from "../build/typechain/contracts/ServiceRegistryFactory";
+import { GraphTokenFactory } from "../build/typechain/contracts/GraphTokenFactory";
+import { CurationFactory } from "../build/typechain/contracts/CurationFactory";
+
+// TODO - make addresses depend on our npm package
 let addresses = (JSON.parse(
-  fs.readFileSync(path.join(__dirname, '..', 'addresses.json'), 'utf-8'),
-) as any).ropsten
-let privateKey = fs
-  .readFileSync(path.join(__dirname, '..', '.privkey.txt'), 'utf-8')
-  .trim()
-let infuraKey = fs
-  .readFileSync(path.join(__dirname, '..', '.infurakey.txt'), 'utf-8')
-  .trim()
+  fs.readFileSync(path.join(__dirname, "..", "addresses.json"), "utf-8")
+) as any).kovan;
+let privateKey = fs.readFileSync(path.join(__dirname, "..", ".privkey.txt"), "utf-8").trim();
+let infuraKey = fs.readFileSync(path.join(__dirname, "..", ".infurakey.txt"), "utf-8").trim();
 
-let ethereum = `https://ropsten.infura.io/v3/${infuraKey}`
-let eth = new ethers.providers.JsonRpcProvider(ethereum)
-let wallet = Wallet.fromMnemonic(privateKey)
-wallet = wallet.connect(eth)
+let ethereum = `https://kovan.infura.io/v3/${infuraKey}`;
+let eth = new ethers.providers.JsonRpcProvider(ethereum);
+let wallet = Wallet.fromMnemonic(privateKey);
+wallet = wallet.connect(eth);
+
+
 
 export const contracts = {
-  gns: GNSFactory.connect(addresses.GNS, wallet),
-  staking: StakingFactory.connect(addresses.Staking, wallet),
-  serviceRegistry: ServiceRegistryFactory.connect(addresses.ServiceRegistry, wallet),
-  graphToken: GraphTokenFactory.connect(addresses.GraphToken, wallet),
-}
+  gns: GnsFactory.connect(addresses.gns, wallet),
+  staking: StakingFactory.connect(addresses.staking, wallet),
+  serviceRegistry: ServiceRegistryFactory.connect(addresses.serviceRegistry, wallet),
+  graphToken: GraphTokenFactory.connect(addresses.graphToken, wallet),
+  curation: CurationFactory.connect(addresses.curation, wallet)
+};
 
-export const createIpfsClient = (node: string) => {
-  let url: URL
-  try {
-    url = new URL(node)
-  } catch (e) {
-    throw new Error(
-      `Invalid IPFS URL: ${node}. ` +
-        `The URL must be of the following format: http(s)://host[:port]/[path]`,
-    )
+export const  executeTransaction = async (
+    transaction: Promise<ContractTransaction>
+      ): Promise<ContractReceipt> => {
+    let tx = await transaction;
+    console.log(`Transaction pending: 'https://kovan.etherscan.io/tx/${tx.hash}'`);
+    let receipt = await tx.wait(1);
+    console.log(`Transaction successfully included in block #${receipt.blockNumber}`);
+    return receipt;
   }
 
-  return ipfsHttpClient({
-    protocol: url.protocol.replace(/[:]+$/, ''),
-    host: url.hostname,
-    port: url.port,
-    'api-path': url.pathname.replace(/\/$/, '') + '/api/v0/',
-  })
+export const overrides = async (contract: string, func: string) => {
+  const gasPrice = utils.parseUnits('25', 'gwei')
+  const gasLimit = 1000000
+  // console.log(`\ntx gas price: '${gasPrice}'`);
+  // console.log(`tx gas limit: ${gasLimit}`);
+
+  return {
+    gasPrice: gasPrice,
+    gasLimit: gasLimit
+  }
+
+  // TODO - make this unique to each function, but for now just passing 1,000,000
+  // const multiplier = utils.bigNumberify(1.5)
+  // switch (contract) {
+  //   case 'gns':
+  //     if (func == 'publish'){
+  //       return {
+  //         gasLimit: (await contracts.gns.estimate.publish(subgraphName, subgraphIDBytes, metaHashBytes)).mul(
+  //           utils.bigNumberify(2))
+  //       }
+  //     }
+  // }
 }
 
-export const ipfsHashToBytes32 = (hash: string) => bs58.decode(hash).slice(2)
 
-export const subgraphNameToDomainHash = (subgraphName: string) =>
-  utils.solidityKeccak256(
-    ['bytes', 'bytes'],
-    [
-      utils.solidityKeccak256(['string'], [subgraphName.split('/')[1]]),
-      utils.solidityKeccak256(['string'], [subgraphName.split('/')[0]]),
-    ],
-  )
+export class IPFS {
+  static createIpfsClient(node: string): ipfsHttpClient {
+    let url: URL;
+    try {
+      url = new URL(node);
+    } catch (e) {
+      throw new Error(
+        `Invalid IPFS URL: ${node}. ` +
+          `The URL must be of the following format: http(s)://host[:port]/[path]`
+      );
+    }
+
+    return ipfsHttpClient({
+      protocol: url.protocol.replace(/[:]+$/, ""),
+      host: url.hostname,
+      port: url.port,
+      "api-path": url.pathname.replace(/\/$/, "") + "/api/v0/"
+    });
+  }
+
+  static ipfsHashToBytes32(hash: string): Buffer {
+    let hashBytes = bs58.decode(hash).slice(2);
+    // console.log(`base58 to bytes32: ${hash} -> ${utils.hexlify(hashBytes)}`);
+    return hashBytes
+  }
+}
