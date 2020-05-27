e072bf4a4cb52aab67ab8c45cee4e72fd7a482f3
LayneHaber <layne.haber@gmail.com>
2020-05-27 11:07:42 -0700

    correct compiler version

--- buidler.config.js
+++ buidler.config.js
@@ -1,27 +1,27 @@
-const buidler = require("@nomiclabs/buidler/config");
-const usePlugin = buidler.usePlugin;
+const buidler = require('@nomiclabs/buidler/config')
+const usePlugin = buidler.usePlugin
 
-usePlugin("@nomiclabs/buidler-truffle5");
+usePlugin('@nomiclabs/buidler-truffle5')
 
 const config = {
   paths: {
-    sources: "./contracts",
-    tests: "./test",
-    artifacts: "./build",
+    sources: './contracts',
+    tests: './test',
+    artifacts: './build',
   },
   solc: {
-    version: "0.6.7", // Note that this only has the version number
+    version: '0.6.4', // Note that this only has the version number
   },
-  defaultNetwork: "buidlerevm",
+  defaultNetwork: 'buidlerevm',
   networks: {
     ganache: {
       chainId: 4447,
-      url: "http://localhost:8545",
+      url: 'http://localhost:8545',
     },
     buidlerevm: {
       loggingEnabled: false,
     },
   },
-};
+}
 
-module.exports = config;
+module.exports = config
