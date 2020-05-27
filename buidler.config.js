ad1637dd82cee83dddd1f5fa72feddecfa19d30d
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-05-27 20:37:51 +0400

    try to add buidler

---
+++ buidler.config.js
@@ -0,0 +1,27 @@
+const buidler = require("@nomiclabs/buidler/config");
+const usePlugin = buidler.usePlugin;
+
+usePlugin("@nomiclabs/buidler-truffle5");
+
+const config = {
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    artifacts: "./build",
+  },
+  solc: {
+    version: "0.6.7", // Note that this only has the version number
+  },
+  defaultNetwork: "buidlerevm",
+  networks: {
+    ganache: {
+      chainId: 4447,
+      url: "http://localhost:8545",
+    },
+    buidlerevm: {
+      loggingEnabled: false,
+    },
+  },
+};
+
+module.exports = config;
