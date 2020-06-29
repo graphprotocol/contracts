19da230a6c50a4984fcb3037f1f7786327971cea
Ariel Barmat <abarmat@gmail.com>
2020-06-29 11:18:13 -0300

    build: add script to verify contracts from address-book in Etherscan (#236)
    * build: add script to verify contracts from address-book in Etherscan
    * build: ignore if contract address is not defined

---
+++ scripts/cli/commands/verify.ts
@@ -0,0 +1,65 @@
+import { Wallet, constants, utils, ContractTransaction } from 'ethers'
+
+import { Argv } from 'yargs'
+
+import { getAddressBook } from '../address-book'
+import { getProvider } from '../utils'
+import { spawn, execSync, spawnSync } from 'child_process'
+
+const coreContracts = [
+  'EpochManager',
+  'GNS',
+  'GraphToken',
+  'ServiceRegistry',
+  'Curation',
+  'RewardsManager',
+  'Staking',
+  'DisputeManager',
+  'IndexerCTDT',
+  'IndexerSingleAssetInterpreter',
+  'IndexerMultiAssetInterpreter',
+  'IndexerWithdrawInterpreter',
+  'MinimumViableMultisig',
+]
+
+export const verify = async (wallet: Wallet, addressBookPath: string): Promise<void> => {
+  const chainId = (await wallet.provider.getNetwork()).chainId
+  const addressBook = getAddressBook(addressBookPath, chainId.toString())
+  console.log(
+    `* Verifying contracts for chainId (${chainId}) using address-book (${addressBookPath})`,
+  )
+
+  for (const contractName of coreContracts) {
+    const contract = addressBook.getEntry(contractName)
+    if (!contract) {
+      console.log(
+        `- ERROR: Contract ${contractName} not found in address-book for network ${chainId}`,
+      )
+      continue
+    }
+
+    const address = contract.address
+    const args = contract.constructorArgs ? contract.constructorArgs.map(e => e.value) : []
+    const argsList = args.map(e => `"${e}"`).join(' ')
+    const cmd = `buidler verify-contract --contract-name ${contractName} --address ${address} ${argsList}`
+
+    try {
+      console.log(`> Verifying contract ${contractName}::${address} ...`)
+      await execSync(cmd)
+      console.log(`+ Contract ${contractName}::${address} verified`)
+    } catch (err) {
+      console.log(`- ERROR: ${contractName}::${address}`)
+    }
+  }
+}
+
+export const verifyCommand = {
+  command: 'verify',
+  describe: 'Verify contracts',
+  handler: async (argv: { [key: string]: any } & Argv['argv']) => {
+    await verify(
+      Wallet.fromMnemonic(argv.mnemonic).connect(getProvider(argv.ethProvider)),
+      argv.addressBook,
+    )
+  },
+}
