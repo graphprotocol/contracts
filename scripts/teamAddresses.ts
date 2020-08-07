07fd0120b5cfa8a30239389baff9f43b1f804625
David Kajpust <kajpustd@gmail.com>
2020-08-07 11:38:44 -0400

    Simplify team addresses ts object

--- scripts/teamAddresses.ts
+++ scripts/teamAddresses.ts
@@ -1,27 +1,18 @@
-type TeamAddresses = {
-  indexer1: string
-  indexer2: string
-  fisherman: string
-  ford: string
-  jannis: string
-  nena: string
-  eva: string
-  ariel: string
-  dave: string
-  zac: string
-  martin: string
+interface TeamMember {
+  name: string
+  address: string
 }
 
-export const teamAddresses: TeamAddresses = {
-  indexer1: '0x8Df7727e3B5aCC57Be09578dc54CDD53a9dA156C',
-  indexer2: '0x0E322262016E94EBE6b17e8396C00D04538b3aED',
-  fisherman: '0x04604EcE79E9E0eeE7B1C262223614e02047B74f',
-  ford: '0xA8B2B5c22E5c13E9F789284b067736D906A5AFa9',
-  jannis: '0x85FAC3f0734Ac360712caED2C0b9782133ed37Da',
-  nena: '0xbEb1Faa6E7e39c7d9BdaB03a7a362fE9d73D7C61',
-  eva: '0x1b037167C4b0584Ca5Ef6534648C38F496757FA5',
-  ariel: '0xeF38F892E4722152fD8eDb50cD84a96344FD47Ce',
-  dave: '0x93606b27cB5e4c780883eC4F6b7Bed5f6572d1dd',
-  zac: '0xD31bC1e2a214066Bb2258dac5f43Ce75e5542Ab9',
-  martin: '0x055BCF2c2BC965Ac8EEAe3f95922D65EE45d3366',
-}
+export const teamAddresses: Array<TeamMember> = [
+  { name: 'indexer1', address: '0x8Df7727e3B5aCC57Be09578dc54CDD53a9dA156C' },
+  { name: 'indexer2', address: '0x0E322262016E94EBE6b17e8396C00D04538b3aED' },
+  { name: 'fisherman', address: '0x04604EcE79E9E0eeE7B1C262223614e02047B74f' },
+  { name: 'ford', address: '0xA8B2B5c22E5c13E9F789284b067736D906A5AFa9' },
+  { name: 'jannis', address: '0x85FAC3f0734Ac360712caED2C0b9782133ed37Da' },
+  { name: 'nena', address: '0xbEb1Faa6E7e39c7d9BdaB03a7a362fE9d73D7C61' },
+  { name: 'eva', address: '0x1b037167C4b0584Ca5Ef6534648C38F496757FA5' },
+  { name: 'ariel', address: '0xeF38F892E4722152fD8eDb50cD84a96344FD47Ce' },
+  { name: 'dave', address: '0x93606b27cB5e4c780883eC4F6b7Bed5f6572d1dd' },
+  { name: 'zac', address: '0xD31bC1e2a214066Bb2258dac5f43Ce75e5542Ab9' },
+  { name: 'martin', address: '0x055BCF2c2BC965Ac8EEAe3f95922D65EE45d3366' },
+]
