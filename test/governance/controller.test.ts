02d1024f1ec8eab8d8924ce9246fdea78f519d95
Ariel Barmat <abarmat@gmail.com>
2020-08-25 01:46:28 -0300

    Use function provided by ethers for hashing names

--- test/governance/controller.test.ts
+++ test/governance/controller.test.ts
@@ -1,9 +1,10 @@
 import { expect } from 'chai'
+import { utils } from 'ethers'
 
 import { Controller } from '../../build/typechain/contracts/Controller'
 import { EpochManager } from '../../build/typechain/contracts/EpochManager'
 
-import { getAccounts, Account, stringToBytes32 } from '../lib/testHelpers'
+import { getAccounts, Account } from '../lib/testHelpers'
 import { NetworkFixture } from '../lib/fixtures'
 
 describe('Manager', () => {
@@ -38,28 +39,28 @@ describe('Manager', () => {
     expect(await epochManager.controller()).eq(controller.address)
 
     // Test the controller
-    const id = stringToBytes32('EpochManager')
+    const id = utils.id('EpochManager')
     const tx = controller.connect(governor.signer).setContractProxy(id, newMockEpochManager.address)
     await expect(tx).emit(controller, 'SetContractProxy').withArgs(id, newMockEpochManager.address)
     expect(await controller.getContractProxy(id)).eq(newMockEpochManager.address)
   })
   it('should fail to set contract proxy if not governor', async function () {
     // Test the controller
-    const id = stringToBytes32('EpochManager')
+    const id = utils.id('EpochManager')
     const tx = controller.connect(me.signer).setContractProxy(id, newMockEpochManager.address)
     await expect(tx).revertedWith('Only Governor can call')
   })
   it('should update controller on a manager', async function () {
     const tx = controller
       .connect(governor.signer)
-      .updateController(stringToBytes32('EpochManager'), mockController.address)
+      .updateController(utils.id('EpochManager'), mockController.address)
     await expect(tx).emit(epochManager, 'SetController').withArgs(mockController.address)
     expect(await epochManager.controller()).eq(mockController.address)
   })
   it('should fail updating controller when not called by governor', async function () {
     const tx = controller
       .connect(me.signer)
-      .updateController(stringToBytes32('EpochManager'), mockController.address)
+      .updateController(utils.id('EpochManager'), mockController.address)
     await expect(tx).revertedWith('Only Governor can call')
   })
   it('should fail setting controller when not called from Controller', async function () {
