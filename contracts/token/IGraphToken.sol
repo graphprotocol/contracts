7df4a917b84d93c77b1f01dd52af3e479ae9eb26
Ariel Barmat <abarmat@gmail.com>
2020-10-02 11:22:33 -0300

    misc: interfaces are missing functions present in their implementations
    Some interfaces in the code base are not were matching their implementation, defining only a part of the functionalities implemented in their respective contracts.

--- contracts/token/IGraphToken.sol
+++ contracts/token/IGraphToken.sol
@@ -3,7 +3,31 @@ pragma solidity ^0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IGraphToken is IERC20 {
+    // -- Mint and Burn --
+
     function burn(uint256 amount) external;
 
     function mint(address _to, uint256 _amount) external;
+
+    // -- Mint Admin --
+
+    function addMinter(address _account) external;
+
+    function removeMinter(address _account) external;
+
+    function renounceMinter() external;
+
+    function isMinter(address _account) external view returns (bool);
+
+    // -- Permit --
+
+    function permit(
+        address _owner,
+        address _spender,
+        uint256 _value,
+        uint256 _deadline,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external;
 }
