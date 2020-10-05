5225c1aee3d0adc9ed07e0a79d2123dfdf93bb59
Ariel Barmat <abarmat@gmail.com>
2020-10-05 10:33:46 -0300

    governance: complete natspec

--- contracts/governance/GraphGovernance.sol
+++ contracts/governance/GraphGovernance.sol
@@ -39,16 +39,32 @@ contract GraphGovernance is GraphGovernanceStorage, GraphUpgradeable, IGraphGove
 
     // -- Proposers --
 
-    function isProposer(address _account) public view returns (bool) {
+    /**
+     * @dev Return whether an account is proposer or not.
+     * @param _account Account to check if it is proposer
+     * @return Return true if the account is proposer
+     */
+    function isProposer(address _account) external override view returns (bool) {
         return _proposers[_account];
     }
 
-    function setProposer(address _account, bool _allowed) public onlyGovernor {
+    /**
+     * @dev Set an account as proposer.
+     * @param _account Account to set as proposerr
+     * @param _allowed True if set as allowed
+     */
+    function setProposer(address _account, bool _allowed) external override onlyGovernor {
         _setProposer(_account, _allowed);
     }
 
-    function setProposers(address[] calldata _accounts, bool[] calldata _alloweds)
-        public
+    /**
+     * @dev Set many accounts as proposers.
+     * @param _accounts List of accounts to change proposer status
+     * @param _alloweds List of booleans for new status of each account
+     */
+    function setProposerMany(address[] calldata _accounts, bool[] calldata _alloweds)
+        external
+        override
         onlyGovernor
     {
         for (uint32 i = 0; i < _accounts.length; i++) {
@@ -56,6 +72,11 @@ contract GraphGovernance is GraphGovernanceStorage, GraphUpgradeable, IGraphGove
         }
     }
 
+    /**
+     * @dev Internal: Set an account as proposer.
+     * @param _account Account to set as proposerr
+     * @param _allowed True if set as allowed
+     */
     function _setProposer(address _account, bool _allowed) internal {
         _proposers[_account] = _allowed;
         emit ProposerUpdated(_account, _allowed);
@@ -63,19 +84,38 @@ contract GraphGovernance is GraphGovernanceStorage, GraphUpgradeable, IGraphGove
 
     // -- Proposals --
 
-    function isProposalCreated(bytes32 _metadata) public view returns (bool) {
+    /**
+     * @dev Get if proposal is created.
+     * @param _metadata Proposal identifier
+     * @return True if the proposal is already created
+     */
+    function isProposalCreated(bytes32 _metadata) public override view returns (bool) {
         return _proposals[_metadata] != ProposalStatus.Null;
     }
 
-    function isProposalResolved(bytes32 _metadata) public view returns (bool) {
+    /**
+     * @dev Get if proposal is resolved.
+     * @param _metadata Proposal identifier
+     * @return True if the proposal is resolved (accepted, rejected)
+     */
+    function isProposalResolved(bytes32 _metadata) public override view returns (bool) {
         return isProposalCreated(_metadata) && _proposals[_metadata] != ProposalStatus.Unresolved;
     }
 
-    function getProposalStatus(bytes32 _metadata) public view returns (ProposalStatus) {
+    /**
+     * @dev Get the proposal status.
+     * @param _metadata Proposal identifier
+     * @return Proposal current status
+     */
+    function getProposalStatus(bytes32 _metadata) external override view returns (ProposalStatus) {
         return _proposals[_metadata];
     }
 
-    function createProposal(bytes32 _metadata) public onlyProposer {
+    /**
+     * @dev Submit a new proposals. Can only be submitted by allowed proposers.
+     * @param _metadata Proposal identifier. This is an IPFS hash to the content of the proposal
+     */
+    function createProposal(bytes32 _metadata) external override onlyProposer {
         require(_metadata != 0x0, "!metadata");
         require(!isProposalCreated(_metadata), "exists");
 
@@ -83,14 +123,22 @@ contract GraphGovernance is GraphGovernanceStorage, GraphUpgradeable, IGraphGove
         emit ProposalCreated(msg.sender, _metadata);
     }
 
-    function approveProposal(bytes32 _metadata) public onlyGovernor {
+    /**
+     * @dev Approve a proposal. Can only by called by the governing multisig.
+     * @param _metadata Proposal identifier. This is an IPFS hash to the content of the proposal
+     */
+    function approveProposal(bytes32 _metadata) external override onlyGovernor {
         require(!isProposalResolved(_metadata), "resolved");
 
         _proposals[_metadata] = ProposalStatus.Approved;
         emit ProposalApproved(msg.sender, _metadata);
     }
 
-    function rejectProposal(bytes32 _metadata) public onlyGovernor {
+    /**
+     * @dev Reject a proposal. Can only by called by the governing multisig.
+     * @param _metadata Proposal identifier. This is an IPFS hash to the content of the proposal
+     */
+    function rejectProposal(bytes32 _metadata) external override onlyGovernor {
         require(!isProposalResolved(_metadata), "resolved");
 
         _proposals[_metadata] = ProposalStatus.Rejected;
--- contracts/governance/IGraphGovernance.sol
+++ contracts/governance/IGraphGovernance.sol
@@ -2,4 +2,26 @@ pragma solidity ^0.6.12;
 
 interface IGraphGovernance {
     enum ProposalStatus { Null, Unresolved, Approved, Rejected }
+
+    // -- Proposers --
+
+    function isProposer(address _account) external view returns (bool);
+
+    function setProposer(address _account, bool _allowed) external;
+
+    function setProposerMany(address[] calldata _accounts, bool[] calldata _alloweds) external;
+
+    // -- Proposals --
+
+    function isProposalCreated(bytes32 _metadata) external view returns (bool);
+
+    function isProposalResolved(bytes32 _metadata) external view returns (bool);
+
+    function getProposalStatus(bytes32 _metadata) external view returns (ProposalStatus);
+
+    function createProposal(bytes32 _metadata) external;
+
+    function approveProposal(bytes32 _metadata) external;
+
+    function rejectProposal(bytes32 _metadata) external;
 }
