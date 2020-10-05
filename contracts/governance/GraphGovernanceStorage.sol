8a1d35d1d83d87bb185ba93ecfd2d3a57e36554a
Ariel Barmat <abarmat@gmail.com>
2020-10-05 10:33:46 -0300

    governance: complete natspec

--- contracts/governance/GraphGovernanceStorage.sol
+++ contracts/governance/GraphGovernanceStorage.sol
@@ -6,6 +6,8 @@ import "./IGraphGovernance.sol";
 contract GraphGovernanceStorage is Governed {
     // -- State --
 
+    // Proposals are identified by a bytes32 IPFS Hash
+    // The hash resource has the content for that proposal
     mapping(bytes32 => IGraphGovernance.ProposalStatus) internal _proposals;
     mapping(address => bool) internal _proposers;
 }
