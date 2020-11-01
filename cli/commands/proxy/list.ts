7217f4b77f27dfc485cd0cf6c332bb740b248708
Ariel Barmat <abarmat@gmail.com>
2020-11-01 02:54:23 -0300

    upgrades: fix upgrade scripts to use ProxyAdmin

--- cli/commands/proxy/list.ts
+++ cli/commands/proxy/list.ts
@@ -13,13 +13,25 @@ export const listProxies = async (cli: CLIEnvironment): Promise<void> => {
     colWidths: [20, 45, 45, 45],
   })
 
+  // Get the proxy admin
+  const proxyAdminEntry = cli.addressBook.getEntry('GraphProxyAdmin')
+  if (!proxyAdminEntry || !proxyAdminEntry.address) {
+    logger.fatal('Missing GraphProxyAdmin configuration')
+    return
+  }
+  const proxyAdmin = getContractAt('GraphProxyAdmin', proxyAdminEntry.address).connect(cli.wallet)
+
+  // Scan every proxy and get current admin and implementation
   for (const contractName of cli.addressBook.listEntries()) {
     const addressEntry = cli.addressBook.getEntry(contractName)
     if (addressEntry.proxy) {
-      const contract = getContractAt('GraphProxy', addressEntry.address).connect(cli.wallet)
-      const implementationAddress = await contract.implementation()
-      const adminAddress = await contract.admin()
-      table.push([contractName, addressEntry.address, implementationAddress, adminAddress])
+      try {
+        const implementationAddress = await proxyAdmin.getProxyImplementation(addressEntry.address)
+        const adminAddress = await proxyAdmin.getProxyAdmin(addressEntry.address)
+        table.push([contractName, addressEntry.address, implementationAddress, adminAddress])
+      } catch {
+        table.push([contractName, addressEntry.address, '', ''])
+      }
     }
   }
 
