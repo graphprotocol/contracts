4dd4cb91887e1c50180eb6765773c0edf86771a5
Ariel Barmat <abarmat@gmail.com>
2020-11-12 00:53:52 -0300

    state-channel: bumped nitro version to fix _transferAsset function override
    This commit also updates IERC20 and SafeMath interfaces as a pre-compile task to be used by both the Graph Network contracts compiled with Solidity 0.7.x and the GRTAssetHolder with Solidity 0.6.x

--- contracts/governance/IController.sol
+++ contracts/governance/IController.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >=0.6.12;
+pragma solidity >=0.6.12 <0.8.0;
 
 interface IController {
     function getGovernor() external view returns (address);
--- contracts/staking/IStaking.sol
+++ contracts/staking/IStaking.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >=0.6.12;
+pragma solidity >=0.6.12 <0.8.0;
 pragma experimental ABIEncoderV2;
 
 interface IStaking {
