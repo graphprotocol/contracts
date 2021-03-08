7b3e850aca085fb482816c2ca0be1c69a67cf0ef
Ariel Barmat <abarmat@gmail.com>
2021-03-07 23:27:48 -0300

    gov: update governance contract to just emit proposal for bookeeping purposes along with voting proof from Snapshot

--- contracts/governance/IGraphGovernance.sol
+++ contracts/governance/IGraphGovernance.sol
@@ -1,27 +1,23 @@
-pragma solidity ^0.6.12;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.3;
 
 interface IGraphGovernance {
-    enum ProposalStatus { Null, Unresolved, Approved, Rejected }
-
-    // -- Proposers --
-
-    function isProposer(address _account) external view returns (bool);
-
-    function setProposer(address _account, bool _allowed) external;
-
-    function setProposerMany(address[] calldata _accounts, bool[] calldata _alloweds) external;
+    enum ProposalResolution { Null, Accepted, Rejected }
 
     // -- Proposals --
 
     function isProposalCreated(bytes32 _metadata) external view returns (bool);
 
-    function isProposalResolved(bytes32 _metadata) external view returns (bool);
+    function createProposal(
+        bytes32 _pid,
+        bytes32 _votesProof,
+        ProposalResolution _resolution
+    ) external;
 
-    function getProposalStatus(bytes32 _metadata) external view returns (ProposalStatus);
-
-    function createProposal(bytes32 _metadata) external;
-
-    function approveProposal(bytes32 _metadata) external;
-
-    function rejectProposal(bytes32 _metadata) external;
+    function updateProposal(
+        bytes32 _pid,
+        bytes32 _votesProof,
+        ProposalResolution _resolution
+    ) external;
 }
