c4ee6d7ae263fb04dc98370777d6abf3049bc879
Ariel Barmat <abarmat@gmail.com>
2021-06-27 17:24:20 -0300

    cli: organize commands to list and claim rebates

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ import '@typechain/hardhat'
 const SKIP_LOAD = process.env.SKIP_LOAD === 'true'
 
 if (!SKIP_LOAD) {
-  ;['contracts', 'misc', 'query', 'deployment'].forEach((folder) => {
+  ;['contracts', 'misc', 'query', 'deployment', 'actions'].forEach((folder) => {
     const tasksPath = path.join(__dirname, 'tasks', folder)
     fs.readdirSync(tasksPath)
       .filter((pth) => pth.includes('.ts'))
