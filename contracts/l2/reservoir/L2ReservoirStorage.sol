873b60051a29f0d61014adb3ca59723fabce7554
Pablo Carranza Vélez <pablo@edgeandnode.com>
2022-06-27 15:31:39 +0300

    fix: provide part of the keeper reward to L2 redeemer

--- contracts/l2/reservoir/L2ReservoirStorage.sol
+++ contracts/l2/reservoir/L2ReservoirStorage.sol
@@ -13,4 +13,6 @@ contract L2ReservoirV1Storage {
 contract L2ReservoirV2Storage is L2ReservoirV1Storage {
     // Fraction of the keeper reward to send to the retryable tx redeemer in L2 (fixed point 1e18)
     uint256 public l2KeeperRewardFraction;
+    // Address of the L1Reservoir on L1, used to check if a ticket was auto-redeemed
+    address public l1ReservoirAddress;
 }
