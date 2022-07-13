061ab3f6fcdad9229de5d3a1f323c473fa0ccef6
Pablo Carranza Vélez <pablo@edgeandnode.com>
2022-07-13 13:35:32 +0200

    fix: document non-expiring permits on L2GraphToken [M-02]

--- contracts/l2/token/L2GraphToken.sol
+++ contracts/l2/token/L2GraphToken.sol
@@ -101,7 +101,7 @@ contract GraphTokenUpgradeable is
      * @param _owner Address of the token holder
      * @param _spender Address of the approved spender
      * @param _value Amount of tokens to approve the spender
-     * @param _deadline Expiration time of the signed permit
+     * @param _deadline Expiration time of the signed permit (if zero, the permit will never expire, so use with caution)
      * @param _v Signature version
      * @param _r Signature r value
      * @param _s Signature s value
