b459fb5568173d2aa607a9e5b9cda53b609ca8dd
Tomás Migone <tomas@edgeandnode.com>
2022-07-27 16:43:25 +0200

    feat: scenario 1 wip
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

---
+++ e2e/scenarios/scenario1.test.ts
@@ -0,0 +1,53 @@
+import { expect } from 'chai'
+import hre from 'hardhat'
+import { Account } from '../../tasks/type-extensions'
+import { fixture } from './scenario1'
+
+describe('Scenario 1', () => {
+  const {
+    contracts: { GraphToken, Staking },
+    getAccounts,
+  } = hre.graph()
+
+  let indexer1: Account
+  let indexer2: Account
+  let curator1: Account
+  let curator2: Account
+  let curator3: Account
+
+  let indexers: Account[] = []
+  let curators: Account[] = []
+
+  before(async () => {
+    ;[indexer1, indexer2, curator1, curator2, curator3] = await getAccounts()
+    indexers = [indexer1, indexer2]
+    curators = [curator1, curator2, curator3]
+  })
+
+  it('Indexer1 GRT balance should match airdropped amount', async function () {
+    const balance = await GraphToken.balanceOf(indexer1.address)
+    expect(balance).eq(fixture.grtAmount.sub(fixture.indexer1.stake))
+  })
+
+  it('Indexer2 GRT balance should match airdropped amount', async function () {
+    const balance = await GraphToken.balanceOf(indexer2.address)
+    expect(balance).eq(fixture.grtAmount.sub(fixture.indexer2.stake))
+  })
+
+  it('Curator GRT balances should match airdropped amounts', async function () {
+    for (const account of curators) {
+      const balance = await GraphToken.balanceOf(account.address)
+      expect(balance).eq(fixture.grtAmount)
+    }
+  })
+
+  it('indexer1 should have tokens staked', async function () {
+    const tokensStaked = (await Staking.stakes(indexer1.address)).tokensStaked
+    expect(tokensStaked).eq(fixture.indexer1.stake)
+  })
+
+  it('indexer2 should have tokens staked', async function () {
+    const tokensStaked = (await Staking.stakes(indexer2.address)).tokensStaked
+    expect(tokensStaked).eq(fixture.indexer2.stake)
+  })
+})
--- tasks/e2e/e2e.ts
+++ tasks/e2e/e2e.ts
@@ -3,9 +3,12 @@ import { HardhatRuntimeEnvironment, TaskArguments } from 'hardhat/types'
 import { TASK_TEST, TASK_RUN } from 'hardhat/builtin-tasks/task-names'
 import glob from 'glob'
 import { cliOpts } from '../../cli/defaults'
+import fs from 'fs'
+import path from 'path'
 
 const CONFIG_TESTS = 'e2e/deployment/config/*.test.ts'
 const INIT_TESTS = 'e2e/deployment/init/*.test.ts'
+const SCENARIOS = 'e2e/scenarios/*[!.test].ts'
 
 // Built-in test & run tasks don't support our arguments
 // we can pass them to GRE via env vars
@@ -18,11 +21,27 @@ task('e2e', 'Run all e2e tests')
   .addParam('graphConfig', cliOpts.graphConfig.description, cliOpts.graphConfig.default)
   .addParam('addressBook', cliOpts.addressBook.description, cliOpts.addressBook.default)
   .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
-    const files = [...new glob.GlobSync(CONFIG_TESTS).found, ...new glob.GlobSync(INIT_TESTS).found]
+    const testFiles = [
+      ...new glob.GlobSync(CONFIG_TESTS).found,
+      ...new glob.GlobSync(INIT_TESTS).found,
+    ]
     setGraphEnvVars(args)
     await hre.run(TASK_TEST, {
-      testFiles: files,
+      testFiles: testFiles,
     })
+
+    // Run scenarios one by one
+    // we don't know how one scenario can affect tests from another one
+    const scenarios = new glob.GlobSync(SCENARIOS).found.map((s) =>
+      path.basename(s, path.extname(s)),
+    )
+    for (const scenario of scenarios) {
+      await hre.run('e2e:scenario', {
+        scenario: scenario,
+        graphConfig: args.graphConfig,
+        addressBook: args.addressBook,
+      })
+    }
   })
 
 task('e2e:config', 'Run deployment configuration e2e tests')
@@ -53,11 +72,26 @@ task('e2e:scenario', 'Run scenario scripts and e2e tests')
   .addParam('addressBook', cliOpts.addressBook.description, cliOpts.addressBook.default)
   .setAction(async (args, hre: HardhatRuntimeEnvironment) => {
     setGraphEnvVars(args)
-    await hre.run(TASK_RUN, {
-      script: `e2e/scenarios/${args.scenario}.ts`,
-      noCompile: true,
-    })
-    await hre.run(TASK_TEST, {
-      testFiles: [`e2e/scenarios/${args.scenario}.test.ts`],
-    })
+
+    console.log(`> Running scenario: ${args.scenario}`)
+
+    const script = `e2e/scenarios/${args.scenario}.ts`
+    const test = `e2e/scenarios/${args.scenario}.test.ts`
+
+    if (fs.existsSync(script)) {
+      await hre.run(TASK_RUN, {
+        script: script,
+        noCompile: true,
+      })
+    } else {
+      console.log(`No script found for scenario ${args.scenario}`)
+    }
+
+    if (fs.existsSync(test)) {
+      await hre.run(TASK_TEST, {
+        testFiles: [test],
+      })
+    } else {
+      throw new Error(`No test found for scenario ${args.scenario}`)
+    }
   })
