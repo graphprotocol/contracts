707b856686a1257f95427acf95a45aed1094709d
Tomás Migone <tomas@edgeandnode.com>
2022-08-23 14:13:11 +0200

    gre: refactor to allow using L1 and L2 simultaneously (#682)
    * feat: refactor GRE to support L1 and L2 simultaneously
    
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

---
+++ gre/type-extensions.d.ts
@@ -0,0 +1,80 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { AddressBook } from '../cli/address-book'
+import { NetworkContracts } from '../cli/contracts'
+
+import { EthersProviderWrapper } from '@nomiclabs/hardhat-ethers/internal/ethers-provider-wrapper'
+
+export interface GraphRuntimeEnvironmentOptions {
+  addressBook?: string
+  l1GraphConfig?: string
+  l2GraphConfig?: string
+  graphConfig?: string
+}
+
+export type AccountNames =
+  | 'arbitrator'
+  | 'governor'
+  | 'authority'
+  | 'availabilityOracle'
+  | 'pauseGuardian'
+  | 'allocationExchangeOwner'
+
+export type NamedAccounts = {
+  [name in AccountNames]: SignerWithAddress
+}
+
+export interface GraphNetworkEnvironment {
+  chainId: number
+  provider: EthersProviderWrapper
+  contracts: NetworkContracts
+  graphConfig: any
+  addressBook: AddressBook
+  getNamedAccounts: () => Promise<NamedAccounts>
+  getTestAccounts: () => Promise<SignerWithAddress[]>
+  getDeployer: () => Promise<SignerWithAddress>
+}
+
+export interface GraphRuntimeEnvironment extends GraphNetworkEnvironment {
+  l1: GraphNetworkEnvironment | null
+  l2: GraphNetworkEnvironment | null
+}
+
+declare module 'hardhat/types/runtime' {
+  export interface HardhatRuntimeEnvironment {
+    graph: (opts?: GraphRuntimeEnvironmentOptions) => GraphRuntimeEnvironment
+  }
+}
+
+declare module 'hardhat/types/config' {
+  export interface HardhatConfig {
+    graph: Omit<GraphRuntimeEnvironmentOptions, 'graphConfig'>
+  }
+
+  export interface HardhatUserConfig {
+    graph: Omit<GraphRuntimeEnvironmentOptions, 'graphConfig'>
+  }
+
+  export interface HardhatNetworkConfig {
+    graphConfig?: string
+  }
+
+  export interface HardhatNetworkUserConfig {
+    graphConfig?: string
+  }
+
+  export interface HttpNetworkConfig {
+    graphConfig?: string
+  }
+
+  export interface HttpNetworkUserConfig {
+    graphConfig?: string
+  }
+
+  export interface ProjectPathsConfig {
+    graph?: string
+  }
+
+  export interface ProjectPathsUserConfig {
+    graph?: string
+  }
+}
