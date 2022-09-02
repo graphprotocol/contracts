f7a5a7a7ccbe544403f74db29907f8125b9fec5b
Tomás Migone <tomas@edgeandnode.com>
2022-09-02 11:39:44 +0200

    feat: add parameter to migrate function so we can set automine on/off
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- tasks/deployment/deploy.ts
+++ tasks/deployment/deploy.ts
@@ -10,7 +10,12 @@ task('migrate', 'Migrate contracts')
   .addParam('graphConfig', cliOpts.graphConfig.description, cliOpts.graphConfig.default)
   .addFlag('skipConfirmation', cliOpts.skipConfirmation.description)
   .addFlag('force', cliOpts.force.description)
+  .addFlag('autoMine', 'Enable auto mining after deployment on local networks')
   .setAction(async (taskArgs, hre) => {
     const accounts = await hre.ethers.getSigners()
-    await migrate(await loadEnv(taskArgs, accounts[0] as unknown as Wallet), taskArgs)
+    await migrate(
+      await loadEnv(taskArgs, accounts[0] as unknown as Wallet),
+      taskArgs,
+      taskArgs.autoMine,
+    )
   })
