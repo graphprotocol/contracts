ff11ec2e42b57f2459c2a8b9aa2b5a89a4e34eb7
Pablo Carranza Velez <pablo@edgeandnode.com>
2022-10-20 16:43:19 -0300

    fix: use separate task to unpause bridge

--- tasks/deployment/unpause.ts
+++ tasks/deployment/unpause.ts
@@ -2,17 +2,28 @@ import { task } from 'hardhat/config'
 import { cliOpts } from '../../cli/defaults'
 import GraphChain from '../../gre/helpers/network'
 
-task('migrate:unpause', 'Unpause protocol and bridge')
+task('migrate:unpause', 'Unpause protocol (except bridge)')
   .addOptionalParam('addressBook', cliOpts.addressBook.description)
   .addOptionalParam('graphConfig', cliOpts.graphConfig.description)
   .setAction(async (taskArgs, hre) => {
     const graph = hre.graph(taskArgs)
     const { governor } = await graph.getNamedAccounts()
-    const { Controller, L1GraphTokenGateway, L2GraphTokenGateway } = graph.contracts
+    const { Controller } = graph.contracts
 
     console.log('> Unpausing protocol')
     const tx = await Controller.connect(governor).setPaused(false)
     await tx.wait()
+
+    console.log('Done!')
+  })
+
+task('migrate:unpause-bridge', 'Unpause bridge')
+  .addOptionalParam('addressBook', cliOpts.addressBook.description)
+  .addOptionalParam('graphConfig', cliOpts.graphConfig.description)
+  .setAction(async (taskArgs, hre) => {
+    const graph = hre.graph(taskArgs)
+    const { governor } = await graph.getNamedAccounts()
+    const { L1GraphTokenGateway, L2GraphTokenGateway } = graph.contracts
 
     console.log('> Unpausing bridge')
     const GraphTokenGateway = GraphChain.isL2(graph.chainId)
