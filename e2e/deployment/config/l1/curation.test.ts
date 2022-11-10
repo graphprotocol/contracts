40ed107ad1a23ef91793de3d5d8dc06ee7da6b41
Pablo Carranza Velez <pablo@edgeandnode.com>
2022-11-10 15:32:27 -0300

    test: fix chain path in e2e

--- e2e/deployment/config/l1/curation.test.ts
+++ e2e/deployment/config/l1/curation.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import hre from 'hardhat'
 import { getItemValue } from '../../../../cli/config'
-import GraphChain from '../../../../gre/helpers/network'
+import GraphChain from '../../../../gre/helpers/chain'
 
 describe('[L1] Curation configuration', () => {
   const graph = hre.graph()
