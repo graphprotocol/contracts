dd38b013c14ff48546282ab148af00933e405aa9
Pablo Carranza Velez <pablo@edgeandnode.com>
2022-11-10 12:23:25 -0300

    test(e2e): separate L1/L2 curation tests

---
+++ e2e/deployment/config/l2/l2Curation.test.ts
@@ -0,0 +1,49 @@
+import { expect } from 'chai'
+import hre from 'hardhat'
+import { getItemValue } from '../../../../cli/config'
+import GraphChain from '../../../../gre/helpers/network'
+
+describe('[L2] L2Curation configuration', () => {
+  const graph = hre.graph()
+  const {
+    graphConfig,
+    contracts: { Controller, L2Curation, BancorFormula, GraphCurationToken },
+  } = graph
+
+  before(async function () {
+    if (GraphChain.isL1(graph.chainId)) this.skip()
+  })
+
+  it('should be controlled by Controller', async function () {
+    const controller = await L2Curation.controller()
+    expect(controller).eq(Controller.address)
+  })
+
+  it('bondingCurve should match the BancorFormula deployment address', async function () {
+    const bondingCurve = await L2Curation.bondingCurve()
+    expect(bondingCurve).eq(BancorFormula.address)
+  })
+
+  it('curationTokenMaster should match the GraphCurationToken deployment address', async function () {
+    const bondingCurve = await L2Curation.curationTokenMaster()
+    expect(bondingCurve).eq(GraphCurationToken.address)
+  })
+
+  it('defaultReserveRatio should be a constant 1000000', async function () {
+    const value = await L2Curation.defaultReserveRatio()
+    const expected = 1000000
+    expect(value).eq(expected)
+  })
+
+  it('curationTaxPercentage should match "curationTaxPercentage" in the config file', async function () {
+    const value = await L2Curation.curationTaxPercentage()
+    const expected = getItemValue(graphConfig, 'contracts/L2Curation/init/curationTaxPercentage')
+    expect(value).eq(expected)
+  })
+
+  it('minimumCurationDeposit should match "minimumCurationDeposit" in the config file', async function () {
+    const value = await L2Curation.minimumCurationDeposit()
+    const expected = getItemValue(graphConfig, 'contracts/L2Curation/init/minimumCurationDeposit')
+    expect(value).eq(expected)
+  })
+})
