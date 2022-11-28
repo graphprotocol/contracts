c62d20946307a5eeeb54b3cdfd05044d381f73ee
Tomás Migone <tomas@edgeandnode.com>
2022-11-28 15:01:32 -0300

    chore: update issuance rate on e2e test
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- e2e/deployment/config/l1/rewardsManager.test.ts
+++ e2e/deployment/config/l1/rewardsManager.test.ts
@@ -12,6 +12,6 @@ describe('[L1] RewardsManager configuration', () => {
 
   it('issuanceRate should match "issuanceRate" in the config file', async function () {
     const value = await RewardsManager.issuanceRate()
-    expect(value).eq('1000000012184945188') // hardcoded as it's set with a function call rather than init parameter
+    expect(value).eq('1000000011247641700') // hardcoded as it's set with a function call rather than init parameter
   })
 })
