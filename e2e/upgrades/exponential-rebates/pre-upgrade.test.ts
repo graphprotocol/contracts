8f2ef3baa5c2d11a2ecae3cfa35ac13cc796dcdc
Tomás Migone <tomas@edgeandnode.com>
2023-05-23 16:59:31 -0300

    feat(upgrade): add allocation state transitions test
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- e2e/upgrades/exponential-rebates/pre-upgrade.test.ts
+++ e2e/upgrades/exponential-rebates/pre-upgrade.test.ts
@@ -4,10 +4,11 @@ import { Contract } from 'ethers'
 import hre from 'hardhat'
 
 import removedABI from './abis/staking'
+import allocations from './fixtures/allocations'
 
 chai.use(chaiAsPromised)
 
-describe('Exponential rebates upgrade', () => {
+describe('[BEFORE UPGRADE] Exponential rebates upgrade', () => {
   const graph = hre.graph()
   const { Staking } = graph.contracts
   const deployedStaking = new Contract(Staking.address, removedABI, graph.provider)
@@ -18,6 +19,16 @@ describe('Exponential rebates upgrade', () => {
     })
     it(`rebates should exist`, async function () {
       await expect(deployedStaking.rebates(123)).to.eventually.be.fulfilled
+    })
+  })
+
+  describe('> Allocation state transitions', () => {
+    it('should validate fixture data on forked chain', async function () {
+      for (const allocation of allocations) {
+        await expect(Staking.getAllocationState(allocation.id)).to.eventually.equal(
+          allocation.state,
+        )
+      }
     })
   })
 })
