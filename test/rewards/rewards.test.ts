4b334cb91737bd77ab030b95b686325b7fda53e2
Pablo Carranza Velez <pablo@edgeandnode.com>
2023-08-03 13:18:18 -0300

    test: remove a leftover setAssetHolder call

--- test/rewards/rewards.test.ts
+++ test/rewards/rewards.test.ts
@@ -976,7 +976,6 @@ describe('Rewards', () => {
 
       // allow the asset holder
       const tokensToCollect = toGRT('10000')
-      await staking.connect(governor.signer).setAssetHolder(assetHolder.address, true)
 
       // signal in two subgraphs in the same block
       const subgraphs = [subgraphDeploymentID1, subgraphDeploymentID2]
