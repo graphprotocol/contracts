7f1431cceaae1b1aec77546794aff8cd476ac468
Miguel de Elias <miguel.deelias@gmail.com>
2024-04-17 12:26:10 -0300

    chore: add GraphEscrow implementation

--- packages/horizon/contracts/GraphEscrow.sol
+++ packages/horizon/contracts/GraphEscrow.sol
@@ -12,10 +12,25 @@ contract GraphEscrow is IGraphEscrow, GraphEscrowStorageV1Storage, GraphDirector
     // -- Errors --
 
     error GraphEscrowNotGraphPayments();
+    error GraphEscrowInputsLengthMismatch();
+    error GraphEscrowInsufficientThawAmount();
+    error GraphEscrowInsufficientAmount(uint256 available, uint256 required);
+    error GraphEscrowNotThawing();
+    error GraphEscrowStillThawing(uint256 currentTimestamp, uint256 thawEndTimestamp);
 
     // -- Events --
 
     event Deposit(address indexed sender, address indexed receiver, uint256 amount);
+    event CancelThaw(address indexed sender, address indexed receiver);
+    event Thaw(
+        address indexed sender,
+        address indexed receiver,
+        uint256 amount,
+        uint256 totalAmountThawing,
+        uint256 thawEndTimestamp
+    );
+    event Withdraw(address indexed sender, address indexed receiver, uint256 amount);
+    event Collect(address indexed sender, address indexed receiver, uint256 amount);
 
     // -- Modifier --
 
@@ -40,14 +55,87 @@ contract GraphEscrow is IGraphEscrow, GraphEscrowStorageV1Storage, GraphDirector
     }
 
     // Deposit funds into the escrow for multiple receivers
-    function depositMany(address[] calldata receivers, uint256[] calldata amounts) external {}
+    function depositMany(address[] calldata receivers, uint256[] calldata amounts) external {
+        if (receivers.length != amounts.length) {
+            revert GraphEscrowInputsLengthMismatch();
+        }
+
+        uint256 totalAmount = 0;
+        for (uint256 i = 0; i < receivers.length; i++) {
+            address receiver = receivers[i];
+            uint256 amount = amounts[i];
+
+            totalAmount += amount;
+            escrowAccounts[msg.sender][receiver].balance += amount;
+            emit Deposit(msg.sender, receiver, amount);
+        }
+
+        graphToken.transferFrom(msg.sender, address(this), totalAmount);
+    }
 
     // Requests to thaw a specific amount of escrow from a receiver's escrow account
-    function thaw(address receiver, uint256 amount) external {}
+    function thaw(address receiver, uint256 amount) external {
+        EscrowAccount storage account = escrowAccounts[msg.sender][receiver];
+        if (amount == 0) {
+            // if amount thawing is zero and requested amount is zero this is an invalid request.
+            // otherwise if amount thawing is greater than zero and requested amount is zero this
+            // is a cancel thaw request.
+            if (account.amountThawing == 0) {
+                revert GraphEscrowInsufficientThawAmount();
+            }
+            account.amountThawing = 0;
+            account.thawEndTimestamp = 0;
+            emit CancelThaw(msg.sender, receiver);
+            return;
+        }
+
+        // Check if the escrow balance is sufficient
+        if (account.balance < amount) {
+            revert GraphEscrowInsufficientAmount({ available: account.balance, required: amount });
+        }
+
+        // Set amount to thaw
+        account.amountThawing = amount;
+        // Set when the thaw is complete (thawing period number of seconds after current timestamp)
+        account.thawEndTimestamp = block.timestamp + withdrawEscrowThawingPeriod;
+
+        emit Thaw(msg.sender, receiver, amount, account.amountThawing, account.thawEndTimestamp);
+    }
 
     // Withdraws all thawed escrow from a receiver's escrow account
-    function withdraw(address receiver) external {}
+    function withdraw(address receiver) external {
+        EscrowAccount storage account = escrowAccounts[msg.sender][receiver];
+        if (account.thawEndTimestamp == 0) {
+            revert GraphEscrowNotThawing();
+        }
+
+        if (account.thawEndTimestamp > block.timestamp) {
+            revert GraphEscrowStillThawing({
+                currentTimestamp: block.timestamp,
+                thawEndTimestamp: account.thawEndTimestamp
+            });
+        }
+
+        // Amount is the minimum between the amount being thawed and the actual balance
+        uint256 amount = account.amountThawing > account.balance ? account.balance : account.amountThawing;
+
+        account.balance -= amount; // Reduce the balance by the withdrawn amount (no underflow risk)
+        account.amountThawing = 0;
+        account.thawEndTimestamp = 0;
+        graphToken.transfer(msg.sender, amount);
+        emit Withdraw(msg.sender, receiver, amount);
+    }
 
     // Collect from escrow (up to amount available in escrow) for a receiver using sender's deposit
-    function collect(address sender, address receiver, uint256 amount) external onlyGraphPayments {}
+    function collect(address sender, address receiver, uint256 amount) external onlyGraphPayments {
+        EscrowAccount storage account = escrowAccounts[sender][receiver];
+        uint256 available = account.balance - account.amountThawing;
+
+        // TODO: should we revert if not enough funds are available?
+        uint256 collectAmount = amount > available ? available : amount;
+
+        account.balance -= collectAmount;
+        graphToken.transfer(msg.sender, collectAmount);
+        emit Collect(sender, receiver, collectAmount);
+    }
 }
--- packages/horizon/test/GraphEscrow.t.sol
+++ packages/horizon/test/GraphEscrow.t.sol
@@ -15,8 +15,9 @@ contract GraphEscrowTest is Test {
 
     Controller controller;
     MockGRTToken token;
+    address graphPayments = address(0xA1);
 
-    address governor = address(0xA1);
+    address governor = address(0xA2);
     uint256 initialSupply = 1000000 ether;
     uint256 withdrawEscrowThawingPeriod = 60;
 
@@ -32,6 +33,7 @@ contract GraphEscrowTest is Test {
 
         vm.startPrank(governor);
         controller.setContractProxy(keccak256("GraphToken"), address(token));
+        controller.setContractProxy(keccak256("GraphPayments"), graphPayments);
         vm.stopPrank();
 
         escrow = new GraphEscrow(address(controller), withdrawEscrowThawingPeriod);
@@ -47,8 +49,76 @@ contract GraphEscrowTest is Test {
         escrow.deposit(receiver, 1000 ether);
         vm.stopPrank();
 
+        (uint256 receiverEscrowBalance,,) = escrow.escrowAccounts(sender, receiver);
+        assertEq(receiverEscrowBalance, 1000 ether);
+    }
+
+    function testDepositMany() public {
+        address otherReceiver = address(0xB3);
+        address[] memory receivers = new address[](2);
+        receivers[0] = receiver;
+        receivers[1] = otherReceiver;
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = 1000 ether;
+        amounts[1] = 2000 ether;
+
+        token.mint(sender, 3000 ether);
+        vm.startPrank(sender);
+        token.approve(address(escrow), 3000 ether);
+        escrow.depositMany(receivers, amounts);
+        vm.stopPrank();
 
         (uint256 receiverEscrowBalance,,) = escrow.escrowAccounts(sender, receiver);
         assertEq(receiverEscrowBalance, 1000 ether);
+
+        (uint256 otherReceiverEscrowBalance,,) = escrow.escrowAccounts(sender, otherReceiver);
+        assertEq(otherReceiverEscrowBalance, 2000 ether);
+    }
+
+    function testThaw() public {
+        token.mint(sender, 1000 ether);
+        vm.startPrank(sender);
+        token.approve(address(escrow), 1000 ether);
+        escrow.deposit(receiver, 1000 ether);
+        escrow.thaw(receiver, 100 ether);
+        vm.stopPrank();
+
+        (, uint256 amountThawing,uint256 thawEndTimestamp) = escrow.escrowAccounts(sender, receiver);
+        assertEq(amountThawing, 100 ether);
+        assertEq(thawEndTimestamp, block.timestamp + withdrawEscrowThawingPeriod);
+    }
+
+    function testWithdraw() public {
+        token.mint(sender, 1000 ether);
+        vm.startPrank(sender);
+        token.approve(address(escrow), 1000 ether);
+        escrow.deposit(receiver, 1000 ether);
+        escrow.thaw(receiver, 100 ether);
+
+        // advance time
+        skip(withdrawEscrowThawingPeriod + 1);
+
+        escrow.withdraw(receiver);
+        vm.stopPrank();
+
+        (uint256 receiverEscrowBalance,,) = escrow.escrowAccounts(sender, receiver);
+        assertEq(receiverEscrowBalance, 900 ether);
+    }
+
+    function testCollect() public {
+        token.mint(sender, 1000 ether);
+        vm.startPrank(sender);
+        token.approve(address(escrow), 1000 ether);
+        escrow.deposit(receiver, 1000 ether);
+        vm.stopPrank();
+
+        vm.prank(graphPayments);
+        escrow.collect(sender, receiver, 100 ether);
+
+        (uint256 receiverEscrowBalance,,) = escrow.escrowAccounts(sender, receiver);
+        assertEq(receiverEscrowBalance, 900 ether);
+        uint256 graphPaymentsBalance = token.balanceOf(graphPayments);
+        assertEq(graphPaymentsBalance, 100 ether);
     }
 }
