d0de8b1d54919546f18ba3fc91a365f01eb8f8be
Miguel de Elias <miguel.deelias@gmail.com>
2024-08-15 18:51:17 -0300

    fix: add PaymentsEscrow functions for collector tracking (OZ M-04)

--- packages/horizon/test/escrow/collector.t.sol
+++ packages/horizon/test/escrow/collector.t.sol
@@ -7,71 +7,112 @@ import { GraphEscrowTest } from "./GraphEscrow.t.sol";
 
 contract GraphEscrowCollectorTest is GraphEscrowTest {
 
+    function _approveCollector(uint256 tokens) internal {
+        escrow.approveCollector(users.verifier, tokens);
+
+        bool authorized = escrow.isCollectorAuthorized(users.gateway, users.verifier);
+        bool isThawing = escrow.isCollectorThawing(users.gateway, users.verifier);
+        uint256 thawEndTimeRemaining = escrow.getCollectorThawTimeRemaining(users.gateway, users.verifier);
+        assertTrue(authorized);
+        assertFalse(isThawing);
+        assertEq(thawEndTimeRemaining, 0);
+    }
+
+    function _thawCollector() internal {
+        escrow.thawCollector(users.verifier);
+
+        bool authorized = escrow.isCollectorAuthorized(users.gateway, users.verifier);
+        bool isThawing = escrow.isCollectorThawing(users.gateway, users.verifier);
+        uint256 thawEndTimeRemaining = escrow.getCollectorThawTimeRemaining(users.gateway, users.verifier);
+        assertTrue(authorized);
+        assertTrue(isThawing);
+        assertEq(thawEndTimeRemaining, revokeCollectorThawingPeriod);
+    }
+
+    function _cancelThawCollector() internal {
+        escrow.cancelThawCollector(users.verifier);
+
+        bool authorized = escrow.isCollectorAuthorized(users.gateway, users.verifier);
+        bool isThawing = escrow.isCollectorThawing(users.gateway, users.verifier);
+        uint256 thawEndTimeRemaining = escrow.getCollectorThawTimeRemaining(users.gateway, users.verifier);
+        assertTrue(authorized);
+        assertFalse(isThawing);
+        assertEq(thawEndTimeRemaining, 0);
+    }
+
+    function _revokeCollector() internal {
+        escrow.revokeCollector(users.verifier);
+
+        bool authorized = escrow.isCollectorAuthorized(users.gateway, users.verifier);
+        bool isThawing = escrow.isCollectorThawing(users.gateway, users.verifier);
+        uint256 thawEndTimeRemaining = escrow.getCollectorThawTimeRemaining(users.gateway, users.verifier);
+        assertFalse(authorized);
+        assertFalse(isThawing);
+        assertEq(thawEndTimeRemaining, 0);
+    }
+
     /*
      * TESTS
      */
 
-    function testCollector_Approve(uint256 amount) public useGateway useCollector(amount) {
-        (bool authorized,, uint256 thawEndTimestamp) = escrow.authorizedCollectors(users.gateway, users.verifier);
-        assertEq(authorized, true);
-        assertEq(thawEndTimestamp, 0);
+    function testCollector_Approve(uint256 tokens) public useGateway {
+        vm.assume(tokens > 0);
+        _approveCollector(tokens);
     }
 
     function testCollector_RevertWhen_ApprovingForSmallerAllowance(
-        uint256 amount,
-        uint256 smallerAmount
-    ) public useGateway useCollector(amount) {
-        vm.assume(smallerAmount < amount);
-        bytes memory expectedError = abi.encodeWithSignature("PaymentsEscrowInconsistentAllowance(uint256,uint256)", amount, smallerAmount);
+        uint256 tokens,
+        uint256 lessTokens
+    ) public useGateway {
+        vm.assume(tokens > 0);
+        _approveCollector(tokens);
+        vm.assume(lessTokens < tokens);
+        bytes memory expectedError = abi.encodeWithSignature("PaymentsEscrowInconsistentAllowance(uint256,uint256)", tokens, lessTokens);
         vm.expectRevert(expectedError);
-        escrow.approveCollector(users.verifier, smallerAmount);
+        escrow.approveCollector(users.verifier, lessTokens);
     }
 
-    function testCollector_Thaw(uint256 amount) public useGateway useCollector(amount) {
-        escrow.thawCollector(users.verifier);
-
-        (bool authorized,, uint256 thawEndTimestamp) = escrow.authorizedCollectors(users.gateway, users.verifier);
-        assertEq(authorized, true);
-        assertEq(thawEndTimestamp, block.timestamp + revokeCollectorThawingPeriod);
+    function testCollector_Thaw(uint256 tokens) public useGateway {
+        vm.assume(tokens > 0);
+        _approveCollector(tokens);
+        _thawCollector();
     }
 
-    function testCollector_CancelThaw(uint256 amount) public useGateway useCollector(amount) {
-        escrow.thawCollector(users.verifier);
-
-        (bool authorized,, uint256 thawEndTimestamp) = escrow.authorizedCollectors(users.gateway, users.verifier);
-        assertEq(authorized, true);
-        assertEq(thawEndTimestamp, block.timestamp + revokeCollectorThawingPeriod);
-
-        escrow.cancelThawCollector(users.verifier);
-
-        (authorized,, thawEndTimestamp) = escrow.authorizedCollectors(users.gateway, users.verifier);
-        assertEq(authorized, true);
-        assertEq(thawEndTimestamp, 0);
+    function testCollector_CancelThaw(uint256 tokens) public useGateway {
+        vm.assume(tokens > 0);
+        _approveCollector(tokens);
+        _thawCollector();
+        _cancelThawCollector();
     }
 
-    function testCollector_RevertWhen_CancelThawIsNotThawing(uint256 amount) public useGateway useCollector(amount) {
+    function testCollector_RevertWhen_CancelThawIsNotThawing(uint256 tokens) public useGateway {
+        vm.assume(tokens > 0);
+        _approveCollector(tokens);
         bytes memory expectedError = abi.encodeWithSignature("PaymentsEscrowNotThawing()");
         vm.expectRevert(expectedError);
         escrow.cancelThawCollector(users.verifier);
         vm.stopPrank();
     }
 
-    function testCollector_Revoke(uint256 amount) public useGateway useCollector(amount) {
-        escrow.thawCollector(users.verifier);
+    function testCollector_Revoke(uint256 tokens) public useGateway {
+        vm.assume(tokens > 0);
+        _approveCollector(tokens);
+        _thawCollector();
         skip(revokeCollectorThawingPeriod + 1);
-        escrow.revokeCollector(users.verifier);
-
-        (bool authorized,,) = escrow.authorizedCollectors(users.gateway, users.verifier);
-        assertEq(authorized, false);
+        _revokeCollector();
     }
 
-    function testCollector_RevertWhen_RevokeIsNotThawing(uint256 amount) public useGateway useCollector(amount) {
+    function testCollector_RevertWhen_RevokeIsNotThawing(uint256 tokens) public useGateway {
+        vm.assume(tokens > 0);
+        _approveCollector(tokens);
         bytes memory expectedError = abi.encodeWithSignature("PaymentsEscrowNotThawing()");
         vm.expectRevert(expectedError);
         escrow.revokeCollector(users.verifier);
     }
 
-    function testCollector_RevertWhen_RevokeIsStillThawing(uint256 amount) public useGateway useCollector(amount) {
+    function testCollector_RevertWhen_RevokeIsStillThawing(uint256 tokens) public useGateway {
+        vm.assume(tokens > 0);
+        _approveCollector(tokens);
         escrow.thawCollector(users.verifier);
         bytes memory expectedError = abi.encodeWithSignature("PaymentsEscrowStillThawing(uint256,uint256)", block.timestamp, block.timestamp + revokeCollectorThawingPeriod);
         vm.expectRevert(expectedError);
