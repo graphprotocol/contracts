5a06dd8697f2a84dbc2b4af5e9e109095f3e344f
Tomás Migone <tomas@edgeandnode.com>
2024-08-22 14:15:44 -0300

    fix: invalid pool detection
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/horizon/contracts/staking/HorizonStakingExtension.sol
+++ packages/horizon/contracts/staking/HorizonStakingExtension.sol
@@ -349,7 +349,7 @@ contract HorizonStakingExtension is HorizonStakingBase, IL2StakingBase, IHorizon
         IHorizonStakingTypes.DelegationInternal storage delegation = pool.delegators[_delegationData.delegator];
 
         // If pool is in an invalid state, return the tokens to the delegator
-        if (pool.tokens == 0 && (pool.shares == 0 || pool.sharesThawing == 0)) {
+        if (pool.tokens == 0 && (pool.shares != 0 || pool.sharesThawing != 0)) {
             _graphToken().transfer(_delegationData.delegator, _tokens);
             emit TransferredDelegationReturnedToDelegator(_delegationData.indexer, _delegationData.delegator, _tokens);
             return;
