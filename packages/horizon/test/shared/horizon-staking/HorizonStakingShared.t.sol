8157a68014e0082de350b0793742ba92742ad974
Tomás Migone <tomas@edgeandnode.com>
2024-09-02 16:36:31 -0300

    test: refactor ttools tests
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/horizon/test/shared/horizon-staking/HorizonStakingShared.t.sol
+++ packages/horizon/test/shared/horizon-staking/HorizonStakingShared.t.sol
@@ -8,6 +8,7 @@ import { IGraphPayments } from "../../../contracts/interfaces/IGraphPayments.sol
 import { IHorizonStakingBase } from "../../../contracts/interfaces/internal/IHorizonStakingBase.sol";
 import { IHorizonStakingMain } from "../../../contracts/interfaces/internal/IHorizonStakingMain.sol";
 import { IHorizonStakingExtension } from "../../../contracts/interfaces/internal/IHorizonStakingExtension.sol";
+import { IL2StakingBase } from "@graphprotocol/contracts/contracts/l2/staking/IL2StakingBase.sol";
 
 import { LinkedList } from "../../../contracts/libraries/LinkedList.sol";
 import { MathUtils } from "../../../contracts/libraries/MathUtils.sol";
@@ -1205,6 +1206,86 @@ abstract contract HorizonStakingSharedTest is GraphBaseTest {
 
         // assert
         assertEq(afterCounterpartStakingAddress, counterpartStakingAddress);
+    }
+
+    function _onTokenTransfer_ReceiveDelegation(address from, uint256 tokens, bytes memory data) internal {
+        (, bytes memory fnData) = abi.decode(data, (uint8, bytes));
+        (address serviceProvider, address delegator) = abi.decode(fnData, (address, address));
+        bytes32 slotPoolTokens = bytes32(uint256(keccak256(abi.encode(serviceProvider, 20))) + 2);
+
+        // before
+        DelegationPool memory beforePool = staking.getDelegationPool(serviceProvider, subgraphDataServiceLegacyAddress);
+        Delegation memory beforeDelegation = staking.getDelegation(
+            serviceProvider,
+            subgraphDataServiceLegacyAddress,
+            delegator
+        );
+        uint256 beforeStoragePoolTokens = uint256(vm.load(address(staking), slotPoolTokens));
+        uint256 beforeDelegatedTokens = staking.getDelegatedTokensAvailable(
+            serviceProvider,
+            subgraphDataServiceLegacyAddress
+        );
+        uint256 beforeDelegatorBalance = token.balanceOf(delegator);
+        uint256 beforeStakingBalance = token.balanceOf(address(staking));
+        uint256 calcShares = (beforePool.tokens == 0 || beforePool.tokens == beforePool.tokensThawing)
+            ? tokens
+            : ((tokens * beforePool.shares) / (beforePool.tokens - beforePool.tokensThawing));
+
+        bool earlyExit = (calcShares == 0 || tokens < 1 ether) ||
+            (beforePool.tokens == 0 && (beforePool.shares != 0 || beforePool.sharesThawing != 0));
+
+        // onTokenTransfer
+        if (earlyExit) {
+            vm.expectEmit();
+            emit Transfer(address(staking), delegator, tokens);
+            vm.expectEmit();
+            emit IL2StakingBase.TransferredDelegationReturnedToDelegator(serviceProvider, delegator, tokens);
+        } else {
+            vm.expectEmit();
+            emit IHorizonStakingExtension.StakeDelegated(serviceProvider, delegator, tokens, calcShares);
+        }
+        staking.onTokenTransfer(from, tokens, data);
+
+        // after
+        DelegationPool memory afterPool = staking.getDelegationPool(serviceProvider, subgraphDataServiceLegacyAddress);
+        Delegation memory afterDelegation = staking.getDelegation(
+            serviceProvider,
+            subgraphDataServiceLegacyAddress,
+            delegator
+        );
+        uint256 afterStoragePoolTokens = uint256(vm.load(address(staking), slotPoolTokens));
+        uint256 afterDelegatedTokens = staking.getDelegatedTokensAvailable(
+            serviceProvider,
+            subgraphDataServiceLegacyAddress
+        );
+        uint256 afterDelegatorBalance = token.balanceOf(delegator);
+        uint256 afterStakingBalance = token.balanceOf(address(staking));
+
+        uint256 deltaShares = afterDelegation.shares - beforeDelegation.shares;
+
+        // assertions
+        if (earlyExit) {
+            assertEq(beforePool.tokens, afterPool.tokens);
+            assertEq(beforePool.shares, afterPool.shares);
+            assertEq(beforePool.tokensThawing, afterPool.tokensThawing);
+            assertEq(beforePool.sharesThawing, afterPool.sharesThawing);
+            assertEq(0, deltaShares);
+            assertEq(beforeDelegatedTokens, afterDelegatedTokens);
+            assertEq(beforeStoragePoolTokens, afterStoragePoolTokens);
+            assertEq(beforeDelegatorBalance + tokens, afterDelegatorBalance);
+            assertEq(beforeStakingBalance - tokens, afterStakingBalance);
+        } else {
+            assertEq(beforePool.tokens + tokens, afterPool.tokens);
+            assertEq(beforePool.shares + calcShares, afterPool.shares);
+            assertEq(beforePool.tokensThawing, afterPool.tokensThawing);
+            assertEq(beforePool.sharesThawing, afterPool.sharesThawing);
+            assertEq(calcShares, deltaShares);
+            assertEq(beforeDelegatedTokens + tokens, afterDelegatedTokens);
+            // Ensure correct slot is being updated, pools are stored in different storage locations for legacy subgraph data service
+            assertEq(beforeStoragePoolTokens + tokens, afterStoragePoolTokens);
+            assertEq(beforeDelegatorBalance, afterDelegatorBalance);
+            assertEq(beforeStakingBalance, afterStakingBalance);
+        }
     }
 
     /*
