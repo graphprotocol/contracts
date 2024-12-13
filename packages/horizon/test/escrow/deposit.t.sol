e1a174f4b714cda84243d371ddbf1c63e6f068de
Tomás Migone <tomas@edgeandnode.com>
2024-12-13 17:19:04 -0300

    fix: couple minor improvments to PaymentsEscrow and some new tests
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/horizon/test/escrow/deposit.t.sol
+++ packages/horizon/test/escrow/deposit.t.sol
@@ -6,13 +6,32 @@ import "forge-std/Test.sol";
 import { GraphEscrowTest } from "./GraphEscrow.t.sol";
 
 contract GraphEscrowDepositTest is GraphEscrowTest {
-
     /*
      * TESTS
      */
 
     function testDeposit_Tokens(uint256 amount) public useGateway useDeposit(amount) {
-        (uint256 indexerEscrowBalance,,) = escrow.escrowAccounts(users.gateway, users.verifier, users.indexer);
+        (uint256 indexerEscrowBalance, , ) = escrow.escrowAccounts(users.gateway, users.verifier, users.indexer);
         assertEq(indexerEscrowBalance, amount);
+    }
+
+    function testDepositTo_Tokens(uint256 amount) public {
+        resetPrank(users.delegator);
+        token.approve(address(escrow), amount);
+        _depositToTokens(users.gateway, users.verifier, users.indexer, amount);
+    }
+
+    // Tests multiple deposits accumulate correctly in the escrow account
+    function testDeposit_MultipleDeposits(uint256 amount1, uint256 amount2) public useGateway {
+        vm.assume(amount1 > 0);
+        vm.assume(amount2 > 0);
+        vm.assume(amount1 <= MAX_STAKING_TOKENS);
+        vm.assume(amount2 <= MAX_STAKING_TOKENS);
+
+        _depositTokens(users.verifier, users.indexer, amount1);
+        _depositTokens(users.verifier, users.indexer, amount2);
+
+        (uint256 balance,,) = escrow.escrowAccounts(users.gateway, users.verifier, users.indexer);
+        assertEq(balance, amount1 + amount2);
     }
 }
