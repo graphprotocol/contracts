c75c894db8a2de1b83c4c8c63745888dc738b9c4
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2025-02-26 03:37:30 -0300

    feat: add extra parameters to QueryFeesCollected event

--- packages/subgraph-service/contracts/interfaces/ISubgraphService.sol
+++ packages/subgraph-service/contracts/interfaces/ISubgraphService.sol
@@ -30,10 +30,19 @@ interface ISubgraphService is IDataServiceFees {
      * @notice Emitted when a subgraph service collects query fees from Graph Payments
      * @param serviceProvider The address of the service provider
      * @param payer The address paying for the query fees
+     * @param allocationId The id of the allocation
+     * @param subgraphDeploymentId The id of the subgraph deployment
      * @param tokensCollected The amount of tokens collected
      * @param tokensCurators The amount of tokens curators receive
      */
-    event QueryFeesCollected(address indexed serviceProvider, address indexed payer, uint256 tokensCollected, uint256 tokensCurators);
+    event QueryFeesCollected(
+        address indexed serviceProvider,
+        address indexed payer,
+        address indexed allocationId,
+        bytes32 indexed subgraphDeploymentId,
+        uint256 tokensCollected,
+        uint256 tokensCurator
+    );
 
     /**
      * @notice Emitted when the stake to fees ratio is set.
