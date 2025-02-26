f7d7c15979e5cc0160af6251ccd1052c51e8d29f
Matias <matias@edgeandnode.com>
2025-02-26 16:19:25 -0300

    f

--- packages/subgraph-service/test/subgraphService/indexing-agreement/IndexingAgreement.t.sol
+++ packages/subgraph-service/test/subgraphService/indexing-agreement/IndexingAgreement.t.sol
@@ -6,6 +6,7 @@ import "forge-std/Test.sol";
 import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import { IGraphPayments } from "@graphprotocol/horizon/contracts/interfaces/IGraphPayments.sol";
 import { IIPCollector } from "@graphprotocol/horizon/contracts/interfaces/IIPCollector.sol";
+import { ProvisionManager } from "@graphprotocol/horizon/contracts/data-service/utilities/ProvisionManager.sol";
 
 import { ISubgraphService } from "../../../contracts/interfaces/ISubgraphService.sol";
 import { SubgraphServiceTest } from "../SubgraphService.t.sol";
@@ -20,7 +21,7 @@ contract SubgraphServiceIndexingAgreementTest is SubgraphServiceTest {
         address rando,
         IIPCollector.SignedIAV calldata signedIAV
     ) public {
-        vm.assume(rando != users.pauseGuardian);
+        vm.assume(_notInUsers(rando));
 
         resetPrank(users.pauseGuardian);
         subgraphService.pause();
@@ -28,5 +29,37 @@ contract SubgraphServiceIndexingAgreementTest is SubgraphServiceTest {
         resetPrank(rando);
         vm.expectRevert(PausableUpgradeable.EnforcedPause.selector);
         subgraphService.acceptIAV(allocationId, signedIAV);
+    }
+
+    function test_SubgraphService_AcceptIAV_Revert_WhenNotAuthorized(
+        address allocationId,
+        address rando,
+        IIPCollector.SignedIAV calldata signedIAV
+    ) public {
+        vm.assume(_notInUsers(rando));
+
+        resetPrank(rando);
+        bytes memory expectedErr = abi.encodeWithSelector(
+            ProvisionManager.ProvisionManagerNotAuthorized.selector,
+            signedIAV.iav.serviceProvider,
+            rando
+        );
+        vm.expectRevert(expectedErr);
+        subgraphService.acceptIAV(allocationId, signedIAV);
+    }
+
+    function _notInUsers(address _candidate) private view returns (bool) {
+        return
+            _candidate != users.governor &&
+            _candidate != users.deployer &&
+            _candidate != users.indexer &&
+            _candidate != users.operator &&
+            _candidate != users.gateway &&
+            _candidate != users.verifier &&
+            _candidate != users.delegator &&
+            _candidate != users.arbitrator &&
+            _candidate != users.fisherman &&
+            _candidate != users.rewardsDestination &&
+            _candidate != users.pauseGuardian;
     }
 }
