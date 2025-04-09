8f79485b8d3681ef5204108aeb277b9dfad1b44a
Tomás Migone <tomas@edgeandnode.com>
2025-04-09 14:06:45 -0300

    chore: remove unnecessary type assertion
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/horizon/test/deployment/GraphTallyCollector.test.ts
+++ packages/horizon/test/deployment/GraphTallyCollector.test.ts
@@ -1,11 +1,12 @@
-import { expect } from 'chai'
 import hre from 'hardhat'
+
+import { expect } from 'chai'
 import { loadConfig } from '@graphprotocol/toolshed/hardhat'
 
 const config = loadConfig('./ignition/configs/', 'migrate', hre.network.name).config
 const graph = hre.graph()
 
-const GraphTallyCollector = graph.horizon!.contracts.GraphTallyCollector
+const GraphTallyCollector = graph.horizon.contracts.GraphTallyCollector
 
 describe('GraphTallyCollector', function () {
   it('should set the right revokeSignerThawingPeriod', async function () {
