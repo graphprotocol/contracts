2180c6823e8344a4692a820cc1c1bb8ab2508d4b
Tomás Migone <tomas@edgeandnode.com>
2025-04-09 15:16:42 -0300

    chore: split transition tasks
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

---
+++ packages/horizon/tasks/transitions/delegation-slashing.ts
@@ -0,0 +1,26 @@
+import { task, types } from 'hardhat/config'
+import { printBanner } from 'hardhat-graph-protocol/sdk'
+import { requireLocalNetwork } from '@graphprotocol/toolshed/hardhat'
+
+task('transition:enable-delegation-slashing', 'Enables delegation slashing in HorizonStaking')
+  .addOptionalParam('governorIndex', 'Derivation path index for the governor account', 0, types.int)
+  .addFlag('skipNetworkCheck', 'Skip the network check (use with caution)')
+  .setAction(async (taskArgs, hre) => {
+    printBanner('ENABLING DELEGATION SLASHING')
+
+    if (!taskArgs.skipNetworkCheck) {
+      requireLocalNetwork(hre)
+    }
+
+    const signers = await hre.ethers.getSigners()
+    const governor = signers[taskArgs.governorIndex]
+    const horizonStaking = hre.graph().horizon.contracts.HorizonStaking
+
+    console.log('Enabling delegation slashing...')
+    const tx = await horizonStaking.connect(governor).setDelegationSlashingEnabled()
+    await tx.wait()
+
+    // Log if the delegation slashing is enabled
+    const delegationSlashingEnabled = await horizonStaking.isDelegationSlashingEnabled()
+    console.log('Delegation slashing enabled:', delegationSlashingEnabled)
+  })
---
+++ packages/horizon/tasks/transitions/thawing-period.ts
@@ -0,0 +1,23 @@
+import { task, types } from 'hardhat/config'
+import { printBanner } from 'hardhat-graph-protocol/sdk'
+import { requireLocalNetwork } from '@graphprotocol/toolshed/hardhat'
+
+task('transition:clear-thawing', 'Clears the thawing period in HorizonStaking')
+  .addOptionalParam('governorIndex', 'Derivation path index for the governor account', 0, types.int)
+  .addFlag('skipNetworkCheck', 'Skip the network check (use with caution)')
+  .setAction(async (taskArgs, hre) => {
+    printBanner('CLEARING THAWING PERIOD')
+
+    if (!taskArgs.skipNetworkCheck) {
+      requireLocalNetwork(hre)
+    }
+
+    const signers = await hre.ethers.getSigners()
+    const governor = signers[taskArgs.governorIndex]
+    const horizonStaking = hre.graph().horizon.contracts.HorizonStaking
+
+    console.log('Clearing thawing period...')
+    const tx = await horizonStaking.connect(governor).clearThawingPeriod()
+    await tx.wait()
+    console.log('Thawing period cleared')
+  })
