23e3838f3ef392a2453f688a2e14e2ad219dce34
Tomás Migone <tomas@edgeandnode.com>
2025-04-11 12:48:34 -0300

    chore: remove sdk
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/sdk/src/deployments/network/actions/types.ts
+++
@@ -1,8 +0,0 @@
-import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import type { GraphNetworkContracts } from '../deployment/contracts/load'
-
-export type GraphNetworkAction<A, R = void> = (
-  contracts: GraphNetworkContracts,
-  signer: SignerWithAddress,
-  args: A,
-) => Promise<R>
--- packages/sdk/src/utils/address.ts
+++
@@ -1,4 +0,0 @@
-import { getAddress } from 'ethers/lib/utils'
-import { randomHexBytes } from './bytes'
-
-export const randomAddress = (): string => getAddress(randomHexBytes(20))
