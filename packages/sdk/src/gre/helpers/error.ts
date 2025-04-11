9fa584e1ad0ff2e9569269f1e1afe484f1e40a00
Tomás Migone <tomas@edgeandnode.com>
2025-04-11 12:48:34 -0300

    chore: remove sdk
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/sdk/src/gre/helpers/error.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatPluginError } from 'hardhat/plugins'
-import { logError } from './logger'
-
-export class GREPluginError extends HardhatPluginError {
-  constructor(message: string) {
-    super('GraphRuntimeEnvironment', message)
-    logError(message)
-  }
-}
