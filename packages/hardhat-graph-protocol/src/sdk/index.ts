1008dd7e9f5b5b52523ccbd32b88b5a5c299e877
Miguel de Elias <miguel@edgeandnode.com>
2025-04-14 18:13:51 -0300

    chore: added integration tests for disputes

--- packages/hardhat-graph-protocol/src/sdk/index.ts
+++ packages/hardhat-graph-protocol/src/sdk/index.ts
@@ -1,8 +1,10 @@
+import { createAttestationData } from './deployments/subgraph-service/utils/attestation'
 import { generateAllocationProof } from './deployments/subgraph-service/utils/allocation'
 import { getSignedRAVCalldata, getSignerProof } from './deployments/subgraph-service/utils/collection'
 import { SubgraphServiceActions } from './deployments/subgraph-service/actions/subgraphService'
 
 export {
+  createAttestationData,
   generateAllocationProof,
   getSignedRAVCalldata,
   getSignerProof,
