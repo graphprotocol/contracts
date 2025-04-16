bfafec0d454795ffea13a751edc2c0c4cadaa281
Tomás Migone <tomas@edgeandnode.com>
2025-04-16 17:53:54 -0300

    fix: type in comment
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/subgraph-service/contracts/SubgraphService.sol
+++ packages/subgraph-service/contracts/SubgraphService.sol
@@ -103,7 +103,7 @@ contract SubgraphService is
      *
      * @param indexer The address of the indexer to register
      * @param data Encoded registration data:
-     *  - address `url`: The URL of the indexer
+     *  - string `url`: The URL of the indexer
      *  - string `geohash`: The geohash of the indexer
      *  - address `rewardsDestination`: The address where the indexer wants to receive indexing rewards.
      *    Use zero address for automatic reprovisioning to the subgraph service.
