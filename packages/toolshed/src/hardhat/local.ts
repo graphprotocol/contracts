c42b25f301c0f08c1cb9981d88288175c1e38f94
Tomás Migone <tomas@edgeandnode.com>
2025-04-21 15:34:51 -0300

    feat: add legacy allo proof encoding
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/toolshed/src/hardhat/local.ts
+++ packages/toolshed/src/hardhat/local.ts
@@ -1,4 +1,5 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import type { HardhatEthersProvider } from '@nomicfoundation/hardhat-ethers/internal/hardhat-ethers-provider'
+import type { HardhatRuntimeEnvironment } from 'hardhat/types'
 
 const localNetworks = ['localhost', 'hardhat', 'localNetwork']
 
@@ -6,4 +7,13 @@ export function requireLocalNetwork(hre: HardhatRuntimeEnvironment) {
   if (!localNetworks.includes(hre.network.name)) {
     throw new Error(`Network ${hre.network.name} is not a local network.`)
   }
+}
+
+export async function warp(provider: HardhatEthersProvider, seconds: number) {
+  await provider.send('evm_increaseTime', [seconds])
+  await provider.send('evm_mine', [])
+}
+
+export async function mine(provider: HardhatEthersProvider, blocks: number) {
+  await provider.send('evm_mine', [blocks])
 }
