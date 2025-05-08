0906c6e03dec675195c1ded26b63c2c3f7876d9a
Tomás Migone <tomas@edgeandnode.com>
2025-05-08 12:57:32 -0300

    chore: add more integration tests
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

---
+++ packages/subgraph-service/test/integration/after-transition-period/pause.test.ts
@@ -0,0 +1,43 @@
+import hre from 'hardhat'
+
+import { ethers } from 'hardhat'
+import { expect } from 'chai'
+
+import type { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers'
+
+describe('Pausing', () => {
+  let snapshotId: string
+
+  // Test addresses
+  let pauseGuardian: HardhatEthersSigner
+  let governor: HardhatEthersSigner
+  const graph = hre.graph()
+  const subgraphService = graph.subgraphService.contracts.SubgraphService
+
+  before(async () => {
+    pauseGuardian = await graph.accounts.getPauseGuardian()
+    governor = await graph.accounts.getGovernor()
+  })
+
+  beforeEach(async () => {
+    // Take a snapshot before each test
+    snapshotId = await ethers.provider.send('evm_snapshot', [])
+  })
+
+  afterEach(async () => {
+    // Revert to the snapshot after each test
+    await ethers.provider.send('evm_revert', [snapshotId])
+  })
+
+  describe('SubgraphService', () => {
+    it('should be pausable by pause guardian', async () => {
+      await subgraphService.connect(pauseGuardian).pause()
+      expect(await subgraphService.paused()).to.equal(true)
+    })
+
+    it('should be pausable by governor', async () => {
+      await subgraphService.connect(governor).pause()
+      expect(await subgraphService.paused()).to.equal(true)
+    })
+  })
+})
