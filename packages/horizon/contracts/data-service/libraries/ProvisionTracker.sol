b767d809805dfa133ca9b6261ae865eabcd78de2
Matias <matias@edgeandnode.com>
2025-06-04 15:17:35 -0300

    f: revert ProvisionTRacker

--- packages/horizon/contracts/data-service/libraries/ProvisionTracker.sol
+++ packages/horizon/contracts/data-service/libraries/ProvisionTracker.sol
@@ -37,7 +37,7 @@ library ProvisionTracker {
         address serviceProvider,
         uint256 tokens,
         uint32 delegationRatio
-    ) external {
+    ) internal {
         if (tokens == 0) return;
 
         uint256 tokensRequired = self[serviceProvider] + tokens;
@@ -54,7 +54,7 @@ library ProvisionTracker {
      * @param serviceProvider The service provider address
      * @param tokens The amount of tokens to release
      */
-    function release(mapping(address => uint256) storage self, address serviceProvider, uint256 tokens) external {
+    function release(mapping(address => uint256) storage self, address serviceProvider, uint256 tokens) internal {
         if (tokens == 0) return;
         require(self[serviceProvider] >= tokens, ProvisionTrackerInsufficientTokens(self[serviceProvider], tokens));
         self[serviceProvider] -= tokens;
@@ -73,7 +73,7 @@ library ProvisionTracker {
         IHorizonStaking graphStaking,
         address serviceProvider,
         uint32 delegationRatio
-    ) external view returns (bool) {
+    ) internal view returns (bool) {
         uint256 tokensAvailable = graphStaking.getTokensAvailable(serviceProvider, address(this), delegationRatio);
         return self[serviceProvider] <= tokensAvailable;
     }
