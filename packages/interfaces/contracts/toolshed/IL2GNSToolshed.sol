f22e83a680521434ae2d96ce588243d2f9d583b0
Miguel de Elias <miguel@edgeandnode.com>
2025-08-04 17:44:53 -0300

    fix: add missing nextAccountSeqID and multicall to IL2GNSToolshed interface

--- packages/interfaces/contracts/toolshed/IL2GNSToolshed.sol
+++ packages/interfaces/contracts/toolshed/IL2GNSToolshed.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.7.6 || 0.8.27;
 
 import { IGNS } from "../contracts/discovery/IGNS.sol";
 import { IL2GNS } from "../contracts/l2/discovery/IL2GNS.sol";
+import { IMulticall } from "../contracts/base/IMulticall.sol";
 
-interface IL2GNSToolshed is IGNS, IL2GNS {
+interface IL2GNSToolshed is IGNS, IL2GNS, IMulticall {
+    function nextAccountSeqID(address account) external view returns (uint256);
     function subgraphNFT() external view returns (address);
 }
