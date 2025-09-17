89373136cd3cafb0fde47ab6879e85587029c5b2
Tomás Migone <tomas@edgeandnode.com>
2025-09-17 13:41:11 -0300

    chore: add ignition files for local network
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/horizon/ignition/modules/migrate/migrate-4.ts
+++ packages/horizon/ignition/modules/migrate/migrate-4.ts
@@ -5,18 +5,9 @@ import { MigrateCurationGovernorModule } from '../periphery/Curation'
 import { MigrateRewardsManagerGovernorModule } from '../periphery/RewardsManager'
 
 export default buildModule('GraphHorizon_Migrate_4', (m) => {
-  const { L2Curation, L2CurationImplementation } = m.useModule(MigrateCurationGovernorModule)
+  m.useModule(MigrateCurationGovernorModule)
+  m.useModule(MigrateRewardsManagerGovernorModule)
+  m.useModule(MigrateHorizonStakingGovernorModule)
 
-  const { RewardsManager, RewardsManagerImplementation } = m.useModule(MigrateRewardsManagerGovernorModule)
-
-  const { HorizonStaking, HorizonStakingImplementation } = m.useModule(MigrateHorizonStakingGovernorModule)
-
-  return {
-    Graph_Proxy_L2Curation: L2Curation,
-    Implementation_L2Curation: L2CurationImplementation,
-    Graph_Proxy_RewardsManager: RewardsManager,
-    Implementation_RewardsManager: RewardsManagerImplementation,
-    Graph_Proxy_HorizonStaking: HorizonStaking,
-    Implementation_HorizonStaking: HorizonStakingImplementation,
-  }
+  return {}
 })
