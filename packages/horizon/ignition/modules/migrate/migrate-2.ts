453aada31573689db7911697f97f952ef5f7547b
Tomás Migone <tomas@edgeandnode.com>
2025-10-06 16:01:58 -0300

    chore: run tests post horizon script 2 for testnet
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- packages/horizon/ignition/modules/migrate/migrate-2.ts
+++ packages/horizon/ignition/modules/migrate/migrate-2.ts
@@ -3,7 +3,7 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 import { MigrateHorizonProxiesGovernorModule } from '../core/HorizonProxies'
 
 export default buildModule('GraphHorizon_Migrate_2', (m) => {
-  m.useModule(MigrateHorizonProxiesGovernorModule)
+  const { Controller } = m.useModule(MigrateHorizonProxiesGovernorModule)
 
-  return {}
+  return { Controller }
 })
