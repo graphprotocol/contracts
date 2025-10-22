1ff621a0845ad2caad89bcfb06216a1f1d4e5ce1
Rembrandt Kuipers <50174308+RembrandtK@users.noreply.github.com>
2025-10-22 20:36:32 +0000

    feat: issuance allocator

---
+++ packages/issuance/contracts/test/allocate/MockERC165.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity 0.8.27;
+
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+/**
+ * @title MockERC165
+ * @author Edge & Node
+ * @dev Minimal implementation of IERC165 for testing
+ * @notice Used to test interface validation - supports only ERC165, not specific interfaces
+ */
+contract MockERC165 is IERC165 {
+    /**
+     * @inheritdoc IERC165
+     */
+    function supportsInterface(bytes4 interfaceId) public pure override returns (bool) {
+        return interfaceId == type(IERC165).interfaceId;
+    }
+}
