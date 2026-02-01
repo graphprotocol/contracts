23b77bce74889325ca18b529b4f3a72d631611fb
Rembrandt Kuipers <50174308+RembrandtK@users.noreply.github.com>
2026-02-01 11:06:01 +0000

    refactor: consolidate public AllocationManager interface
    Move public members from toolshed into IAllocationManager.
    Toolshed interfaces should only aggregate for type generation.
    
    See docs/InterfaceConsolidationPattern.md

--- packages/subgraph-service/contracts/utilities/AllocationManager.sol
+++ packages/subgraph-service/contracts/utilities/AllocationManager.sol
@@ -5,6 +5,7 @@ import { IGraphPayments } from "@graphprotocol/interfaces/contracts/horizon/IGra
 import { IGraphToken } from "@graphprotocol/interfaces/contracts/contracts/token/IGraphToken.sol";
 import { IHorizonStakingTypes } from "@graphprotocol/interfaces/contracts/horizon/internal/IHorizonStakingTypes.sol";
 import { IAllocation } from "@graphprotocol/interfaces/contracts/subgraph-service/internal/IAllocation.sol";
+import { IAllocationManager } from "@graphprotocol/interfaces/contracts/subgraph-service/internal/IAllocationManager.sol";
 import { ILegacyAllocation } from "@graphprotocol/interfaces/contracts/subgraph-service/internal/ILegacyAllocation.sol";
 import { RewardsReclaim } from "@graphprotocol/interfaces/contracts/contracts/rewards/RewardsReclaim.sol";
 
@@ -28,7 +29,12 @@ import { ProvisionTracker } from "@graphprotocol/horizon/contracts/data-service/
  * @custom:security-contact Please email security+contracts@thegraph.com if you find any
  * bugs. We may have an active bug bounty program.
  */
-abstract contract AllocationManager is EIP712Upgradeable, GraphDirectory, AllocationManagerV1Storage {
+abstract contract AllocationManager is
+    IAllocationManager,
+    EIP712Upgradeable,
+    GraphDirectory,
+    AllocationManagerV1Storage
+{
     using ProvisionTracker for mapping(address => uint256);
     using Allocation for mapping(address => IAllocation.State);
     using Allocation for IAllocation.State;
@@ -39,123 +45,9 @@ abstract contract AllocationManager is EIP712Upgradeable, GraphDirectory, Alloca
     ///@dev EIP712 typehash for allocation id proof
     bytes32 private constant EIP712_ALLOCATION_ID_PROOF_TYPEHASH =
         keccak256("AllocationIdProof(address indexer,address allocationId)");
+    // solhint-disable-previous-line gas-small-strings
 
-    /**
-     * @notice Emitted when an indexer creates an allocation
-     * @param indexer The address of the indexer
-     * @param allocationId The id of the allocation
-     * @param subgraphDeploymentId The id of the subgraph deployment
-     * @param tokens The amount of tokens allocated
-     * @param currentEpoch The current epoch
-     */
-    event AllocationCreated(
-        address indexed indexer,
-        address indexed allocationId,
-        bytes32 indexed subgraphDeploymentId,
-        uint256 tokens,
-        uint256 currentEpoch
-    );
-
-    /**
-     * @notice Emitted when an indexer collects indexing rewards for an allocation
-     * @param indexer The address of the indexer
-     * @param allocationId The id of the allocation
-     * @param subgraphDeploymentId The id of the subgraph deployment
-     * @param tokensRewards The amount of tokens collected
-     * @param tokensIndexerRewards The amount of tokens collected for the indexer
-     * @param tokensDelegationRewards The amount of tokens collected for delegators
-     * @param poi The POI presented
-     * @param poiMetadata The metadata associated with the POI
-     * @param currentEpoch The current epoch
-     */
-    event IndexingRewardsCollected(
-        address indexed indexer,
-        address indexed allocationId,
-        bytes32 indexed subgraphDeploymentId,
-        uint256 tokensRewards,
-        uint256 tokensIndexerRewards,
-        uint256 tokensDelegationRewards,
-        bytes32 poi,
-        bytes poiMetadata,
-        uint256 currentEpoch
-    );
-
-    /**
-     * @notice Emitted when an indexer resizes an allocation
-     * @param indexer The address of the indexer
-     * @param allocationId The id of the allocation
-     * @param subgraphDeploymentId The id of the subgraph deployment
-     * @param newTokens The new amount of tokens allocated
-     * @param oldTokens The old amount of tokens allocated
-     */
-    event AllocationResized(
-        address indexed indexer,
-        address indexed allocationId,
-        bytes32 indexed subgraphDeploymentId,
-        uint256 newTokens,
-        uint256 oldTokens
-    );
-
-    /**
-     * @notice Emitted when an indexer closes an allocation
-     * @param indexer The address of the indexer
-     * @param allocationId The id of the allocation
-     * @param subgraphDeploymentId The id of the subgraph deployment
-     * @param tokens The amount of tokens allocated
-     * @param forceClosed Whether the allocation was force closed
-     */
-    event AllocationClosed(
-        address indexed indexer,
-        address indexed allocationId,
-        bytes32 indexed subgraphDeploymentId,
-        uint256 tokens,
-        bool forceClosed
-    );
-
-    /**
-     * @notice Emitted when a legacy allocation is migrated into the subgraph service
-     * @param indexer The address of the indexer
-     * @param allocationId The id of the allocation
-     * @param subgraphDeploymentId The id of the subgraph deployment
-     */
-    event LegacyAllocationMigrated(
-        address indexed indexer,
-        address indexed allocationId,
-        bytes32 indexed subgraphDeploymentId
-    );
-
-    /**
-     * @notice Emitted when the maximum POI staleness is updated
-     * @param maxPOIStaleness The max POI staleness in seconds
-     */
-    event MaxPOIStalenessSet(uint256 maxPOIStaleness);
-
-    /**
-     * @notice Thrown when an allocation proof is invalid
-     * Both `signer` and `allocationId` should match for a valid proof.
-     * @param signer The address that signed the proof
-     * @param allocationId The id of the allocation
-     */
-    error AllocationManagerInvalidAllocationProof(address signer, address allocationId);
-
-    /**
-     * @notice Thrown when attempting to create an allocation with a zero allocation id
-     */
-    error AllocationManagerInvalidZeroAllocationId();
-
-    /**
-     * @notice Thrown when attempting to collect indexing rewards on a closed allocationl
-     * @param allocationId The id of the allocation
-     */
-    error AllocationManagerAllocationClosed(address allocationId);
-
-    /**
-     * @notice Thrown when attempting to resize an allocation with the same size
-     * @param allocationId The id of the allocation
-     * @param tokens The amount of tokens
-     */
-    error AllocationManagerAllocationSameSize(address allocationId, uint256 tokens);
-
+    // forge-lint: disable-next-item(mixed-case-function)
     /**
      * @notice Initializes the contract and parent contracts
      * @param _name The name to use for EIP712 domain separation
