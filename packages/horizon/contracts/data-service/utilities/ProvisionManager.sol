2a81cffe4c72fcec29216ad4977abc81053464e0
Rembrandt Kuipers <50174308+RembrandtK@users.noreply.github.com>
2026-02-27 18:14:23 +0000

    fixup! feat: IDataServiceAgreements interface and SubgraphService integration

--- packages/horizon/contracts/data-service/utilities/ProvisionManager.sol
+++ packages/horizon/contracts/data-service/utilities/ProvisionManager.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.27;
 
-// TODO: Re-enable and fix issues when publishing a new version
 // solhint-disable gas-indexed-events
 // solhint-disable gas-strict-inequalities
 
